b50334e07ad80944956d2f60d910cca3bd3dc3c6
Nikola Metulev <nikolame@microsoft.com>
2019-01-29 21:20:02 -0800

    added dropdown

--- src/components/my-login/my-login.tsx
+++ src/components/my-login/my-login.tsx
@@ -1,4 +1,4 @@
-import { Component, State } from '@stencil/core';
+import { Component, State, Element } from '@stencil/core';
 
 import * as MicrosoftGraph from "@microsoft/microsoft-graph-types"
 import * as Auth from '../../auth/Auth'
@@ -18,6 +18,15 @@ export class MyLogin {
       Auth.onAuthProviderChanged(_ => this.init())
       this.init();
     }
+  }
+
+  componentDidLoad() {
+    this.$dropdownElement = this.$rootElement.shadowRoot.querySelector('.login-dropdown-root');
+    this.dropdownVisible = false;
+  }
+
+  componentDidUpdate() {
+    this.componentDidLoad();
   }
 
   private async init() {
@@ -58,30 +67,44 @@ export class MyLogin {
     }
   }
 
+  toggleDropdown() {
+    if (this.$dropdownElement) {
+      this.dropdownVisible = !this.dropdownVisible;
+      this.$dropdownElement.className = 'login-dropdown-root' +
+        (this.dropdownVisible ? ' show-dropdown' : '');
+    }
+  }
+
   clicked() {
-    if (typeof this.user !== 'undefined') {
-      this.logout();
+    if (this.user) {
+      this.toggleDropdown();
     } else {
       this.login();
     }
   }
 
+  @Element() $rootElement : HTMLElement;
+
+  private $dropdownElement : HTMLElement;
+  private dropdownVisible : boolean = false;
+
   @State() user : MicrosoftGraph.User;
   @State() profileImage: string | ArrayBuffer;
 
   render() {
-    let content = (typeof this.user !== 'undefined') ?
+    let content = this.user ?
         this.renderLogedIn() : this.renderLogedOut();
 
     return <div class='login-root'>
       <button class='login-root-button' onClick={() => {this.clicked()}}>
         {content}
       </button>
+      {this.renderDropDown()}
     </div>
   }
 
   renderLogedOut() {
-    return 'LogIn';
+    return 'Login';
   }
 
   renderLogedIn() {
@@ -103,5 +126,17 @@ export class MyLogin {
     } else {
       return <div class="login-user-no-image">TD</div>
     }
+  }
+
+  renderDropDown() {
+    if (!this.user) {
+      return;
+    }
+
+    return <div class="login-dropdown-root">
+      <div>{this.user.displayName}</div>
+      <div>{this.user.mail}</div>
+      <button onClick={() => {this.logout()}}>Logout</button>
+    </div>
   }
 }
