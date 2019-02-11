6286dfa341e621b27d116aab7e5c2b358000d744
Alexandre Chohfi <azchohfi@hotmail.com>
2019-02-11 14:47:55 -0800

    Added test for my-msal-provider.

--- src/components/my-msal-provider/my-msal-provider.spec.ts
+++ src/components/my-msal-provider/my-msal-provider.spec.ts
@@ -1,8 +1,25 @@
 import { MyMsalProvider } from './my-msal-provider';
+import * as Auth from '../../auth/Auth'
+import { MSALProvider } from '../../auth/MSALProvider';
 
 describe('MyMsalProvider', () => {
   it('clientId should be undefined by default', () => {
     const auth = new MyMsalProvider();
     expect(auth.clientId).toEqual(undefined);
+  });
+
+  it('setting the clientId should init the MSALProvider', async () => {
+    const auth = new MyMsalProvider();
+
+    const spy = jest.spyOn(Auth, "initWithProvider");
+
+    auth.clientId = "abc";
+    auth.validateClientId();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toBeCalledWith(expect.any(MSALProvider));
+    expect(spy).toBeCalledWith(expect.objectContaining({
+      _clientId: "abc"
+    }));
   });
 });
