a00c6afdcd67dbe0666cc3ad486115b0874cffd0
Nikola Metulev <nikolame@microsoft.com>
2019-02-13 13:37:52 -0800

    updated defineitions

---
+++ M365ToolkitComponents/src/global/helpers.d.ts
@@ -0,0 +1,4 @@
+export declare function hasClass(element: HTMLElement, className: string): boolean;
+export declare function addClass(element: HTMLElement, className: string): void;
+export declare function removeClass(element: HTMLElement, className: string): void;
+export declare function toggleClass(element: HTMLElement, className: string): void;
