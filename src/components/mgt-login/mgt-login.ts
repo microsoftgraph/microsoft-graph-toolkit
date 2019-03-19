0dc4ba007d7070fec413a0ed0eadba7b4b2a02e0
Justin Liu <justin.xin.liu@gmail.com>
2019-03-18 18:28:20 -0700

    Updated sign-in style.

--- src/components/mgt-login/mgt-login.ts
+++ src/components/mgt-login/mgt-login.ts
@@ -65,8 +65,8 @@ export class MgtLogin extends LitElement {
     let content = this._user ? this.renderLoggedIn() : this.renderLoggedOut();
 
     return html`
-      <div class="login-root">
-        <button class="login-root-button" @click=${this.clicked}>
+      <div class="root">
+        <button class="sign-in-button" @click=${this.clicked}>
           ${content}
         </button>
         ${this.renderMenu()}
@@ -76,12 +76,10 @@ export class MgtLogin extends LitElement {
 
   renderLoggedOut() {
     return html`
-      <div class="login-signed-out-root">
-        <i class="ms-Icon ms-Icon--AddFriend"></i>
-        <div class="login-signed-out-content">
-          Sign In
-        </div>
-      </div>
+      <i class="sign-in-icon ms-Icon ms-Icon--AddFriend"></i>
+      <span>
+        Sign In
+      </span>
     `;
   }
 
