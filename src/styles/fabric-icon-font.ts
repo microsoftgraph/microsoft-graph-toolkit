894d43d5d2d97b7c949b5a4f7658e3209abbaabc
Nikola Metulev <nikolame@microsoft.com>
2019-03-24 18:35:54 -0700

    removed relative reference to fabric font

--- src/styles/fabric-icon-font.ts
+++ src/styles/fabric-icon-font.ts
@@ -1,4 +1,9 @@
-const fabricFont = document.createElement('link');
-fabricFont.rel = 'stylesheet';
-fabricFont.href = '/src/styles/fabric-icon-font.css';
+const fabricFont = document.createElement('style');
+fabricFont.type = 'text/css';
+fabricFont.appendChild(document.createTextNode(`
+@font-face {
+    font-family: 'FabricMDL2Icons';
+    src: url('https://static2.sharepointonline.com/files/fabric/assets/icons/fabricmdl2icons-2.68.woff2') format('woff2');
+}
+`));
 document.head!.appendChild(fabricFont);
