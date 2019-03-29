be07ec3d58a51e747179122b4492ab7247f4e300
Ben M <v-bemcl@microsoft.com>
2019-03-29 13:11:29 -0700

    Setting up Restructure

--- src/styles/fabric-icon-font.ts
+++
@@ -1,9 +0,0 @@
-const fabricFont = document.createElement('style');
-fabricFont.type = 'text/css';
-fabricFont.appendChild(document.createTextNode(`
-@font-face {
-    font-family: 'FabricMDL2Icons';
-    src: url('https://static2.sharepointonline.com/files/fabric/assets/icons/fabricmdl2icons-2.68.woff2') format('woff2');
-}
-`));
-document.head!.appendChild(fabricFont);
