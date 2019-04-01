5e6db4f64166102b3348c6ece299e5122d82fda6
Ben M <v-bemcl@microsoft.com>
2019-04-01 14:00:40 -0700

    More Updates to build process

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,21 @@ const src_root = "./src";
 const bin_root = "./dist";
 
 const bundle_inputs = `${src_root}/index.ts`;
+const es5_bundle_input = `./es5/index.ts`;
+
 const ui_inputs = `${src_root}/components/ui/ui.ts`;
 const provider_inputs = [
   `${src_root}/components/providers/mgt-mock-provider.ts`,
   `${src_root}/components/providers/mgt-msal-provider.ts`,
   `${src_root}/components/providers/mgt-teams-provider.ts`,
   `${src_root}/components/providers/mgt-wam-provider.ts`
+];
+
+const base_plugins = [
+  commonJS(),
+  resolve({ module: true, jsnext: true }),
+  postcss({ inject: false }),
+  terser({ keep_classnames: true, keep_fnames: true })
 ];
 
 const typescript_es6 = {
@@ -36,56 +45,49 @@ const typescript_es5 = {
   esModuleInterop: true
 };
 
-const base_plugins = [
-  commonJS(),
-  resolve({ module: true, jsnext: true }),
-  postcss({ inject: false }),
-  terser({ keep_classnames: true, keep_fnames: true })
-];
-
-const es6_bundle_config = {
-  input: bundle_inputs,
-  plugins: [...base_plugins, typescript(typescript_es6)],
-  output: {
-    dir: `${bin_root}/es6/bundle`,
-    entryFileNames: "[name].js",
-    format: "esm"
-  }
-};
-
-const es6_components_config = {
-  input: [ui_inputs, ...provider_inputs],
-  plugins: [...base_plugins, typescript(typescript_es6)],
-  output: {
-    dir: `${bin_root}/es6/components`,
-    entryFileNames: "[name].js",
-    format: "esm"
-  }
-};
-
-const es5_bundle_config = {
-  input: bundle_inputs,
-  plugins: [...base_plugins, typescript(typescript_es5)],
-  output: {
-    dir: `${bin_root}/es5/bundle`,
-    name: `index`,
-    entryFileNames: "[name].js",
-    format: "iife"
-  }
-};
-const es5_components_config = {
-  input: [ui_inputs, ...provider_inputs],
-  plugins: [...base_plugins, typescript(typescript_es5)],
-  output: {
-    dir: `${bin_root}/es5/components`,
-    entryFileNames: "[name].js",
-    format: "iife"
-  }
-};
-
 export default [
-  es6_bundle_config,
-  es6_components_config,
-  es5_bundle_config,
-  // es5_components_config
+  // ES6 Bundle
+  {
+    input: bundle_inputs,
+    plugins: [...base_plugins, typescript(typescript_es6)],
+    output: {
+      dir: `${bin_root}/es6/bundle`,
+      entryFileNames: "[name].js",
+      format: "esm"
+    }
+  },
+  // ES6 Individual Components
+  {
+    input: [ui_inputs, ...provider_inputs],
+    plugins: [...base_plugins, typescript(typescript_es6)],
+    output: {
+      dir: `${bin_root}/es6/components`,
+      entryFileNames: "[name].js",
+      format: "esm"
+    }
+  },
+  // ES5 Bundle
+  {
+    input: es5_bundle_input,
+    plugins: [...base_plugins, typescript(typescript_es5)],
+    output: {
+      dir: `${bin_root}/es5/bundle`,
+      name: `index`,
+      entryFileNames: "[name].js",
+      format: "iife"
+    }
+  },
+  // ES5 Individual Components
+  // {
+  //   input: [ui_inputs, ...provider_inputs],
+  //   plugins: [...base_plugins, typescript(typescript_es5)],
+  //   output: {
+  //     dir: `${bin_root}/es5/components`,
+  //     entryFileNames: "[name].js",
+  //     format: "iife"
+  //   }
+  // },
+  {
+    
+  }
 ];
