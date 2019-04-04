fec4b9239ff61d508b3cf60e29087656aa1286cb
Ben M <v-bemcl@microsoft.com>
2019-04-04 13:18:55 -0700

    Got IE11 Builds working

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,44 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonJS from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript";
+import babel from "rollup-plugin-babel";
+
+const extensions = [".js", ".ts"];
 
 const src_root = "./src";
 const bin_root = "./dist";
 
-const bundle_inputs = `${src_root}/index.ts`;
-const es5_bundle_input = `./es5/index.ts`;
-
-const ui_inputs = `${src_root}/components/ui/ui.ts`;
-const provider_inputs = [
-  `${src_root}/components/providers/mgt-mock-provider.ts`,
-  `${src_root}/components/providers/mgt-msal-provider.ts`,
-  `${src_root}/components/providers/mgt-teams-provider.ts`,
-  `${src_root}/components/providers/mgt-wam-provider.ts`
-];
+const es6_bundle_inputs = `${src_root}/index.ts`;
+const es5_bundle_input = `${src_root}/index.es5.ts`;
 
 const base_plugins = [
   commonJS(),
-  resolve({ module: true, jsnext: true }),
+  resolve({ module: true, jsnext: true, extensions }),
   postcss({ inject: false }),
   terser({ keep_classnames: true, keep_fnames: true })
 ];
+
+const babel_es5 = {
+  extensions,
+  presets: ["@babel/env", "@babel/typescript"],
+  plugins: [
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
+    [
+      "@babel/plugin-proposal-decorators",
+      { decoratorsBeforeExport: true, legacy: false }
+    ]
+  ],
+  include: [
+    "src/**/*",
+    "node_modules/lit-element/**/*"
+  ],
+  exclude: [
+    "node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js"
+  ]
+};
 
 const typescript_es6 = {
   target: "es2015",
@@ -36,55 +51,31 @@ const typescript_es6 = {
 };
 
 const typescript_es5 = {
+  ...typescript_es6,
   target: "es5",
-  module: "esnext",
-  moduleResolution: "node",
-  lib: ["dom", "es2015"],
-  allowSyntheticDefaultImports: true,
-  experimentalDecorators: true,
-  esModuleInterop: true
+  module: "esnext"
 };
 
 export default [
   // ES6 Bundle
   {
-    input: bundle_inputs,
-    plugins: [...base_plugins, typescript(typescript_es6)],
+    input: es6_bundle_inputs,
+    plugins: [typescript(typescript_es6), ...base_plugins],
     output: {
       dir: `${bin_root}/es6/bundle`,
-      entryFileNames: "[name].js",
-      format: "esm"
-    }
-  },
-  // ES6 Individual Components
-  {
-    input: [ui_inputs, ...provider_inputs],
-    plugins: [...base_plugins, typescript(typescript_es6)],
-    output: {
-      dir: `${bin_root}/es6/components`,
-      entryFileNames: "[name].js",
+      entryFileNames: "mgt.js",
       format: "esm"
     }
   },
   // ES5 Bundle
   {
     input: es5_bundle_input,
-    plugins: [...base_plugins, typescript(typescript_es5)],
+    plugins: [babel(babel_es5), ...base_plugins],
     output: {
       dir: `${bin_root}/es5/bundle`,
       name: `index`,
-      entryFileNames: "[name].js",
-      format: "esm"
+      entryFileNames: "mgt.js",
+      format: "iife"
     }
-  },
-  // ES5 Individual Components
-  // {
-  //   input: [ui_inputs, ...provider_inputs],
-  //   plugins: [...base_plugins, typescript(typescript_es5)],
-  //   output: {
-  //     dir: `${bin_root}/es5/components`,
-  //     entryFileNames: "[name].js",
-  //     format: "iife"
-  //   }
-  // }
+  }
 ];
---
+++ src/index.es5.ts
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+import 'whatwg-fetch';
+import '../node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js';
+import "@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js";
+
+
+export * from './index';
