5dedbe1c0eb9de71e669cc7f4423faaa3f7cedf1
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2019-05-04 21:52:36 -0700

    Updated tests. (#74)
    * Updated tests.
    
    * Added tests to prettier blog pattern.
    
    * Fix build

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,12 @@
-const esModules = ['msal', '@open-wc', 'lit-html', 'lit-element'].join('|');
+const esModules = ['msal', '@open-wc', 'lit-html', 'lit-element', '@microsoft/microsoft-graph-client'].join('|');
 module.exports = {
   collectCoverage: true,
-  coverageReporters: [
-    "cobertura",
-    "html"
-  ],
-  reporters: ["default", "jest-junit"],
-  testPathIgnorePatterns:
-  [
-    "/samples/"
-  ],
+  coverageReporters: ['cobertura', 'html'],
+  reporters: ['default', 'jest-junit'],
+  testPathIgnorePatterns: ['/samples/'],
   preset: 'ts-jest/presets/js-with-ts',
   transformIgnorePatterns: [`<rootDir>/node_modules/(?!${esModules})`],
-  testMatch: [
-    "**/tests/**/*.tsx",
-  ],
+  testMatch: ['**/tests/**/*.tsx'],
   globals: {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json'
