e0d404f88ad9b43c9222f134528835f36057940b
Nikola Metulev <nikolame@microsoft.com>
2019-05-05 19:10:47 -0700

    updated react sample

--- samples/react-app/src/App.js
+++ samples/react-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import "microsoft-graph-toolkit";
+import "@microsoft/mgt";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <mgt-login ref="loginComponent"></mgt-login>
+          <mgt-agenda group-by-day></mgt-agenda>
+
+          
           <mgt-person show-name ref={el => el.personDetails = {displayName: 'Nikola Metulev'}}></mgt-person>
       </div>
     );
--- samples/react-app/src/index.js
+++ samples/react-app/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Providers, MsalProvider} from '@microsoft/mgt';
+import {MockProvider} from '@microsoft/mgt/dist/es6/mock/MockProvider';
+
+Providers.globalProvider = new MockProvider();
+
+// uncomment to use MSAL provider with your own ClientID (also comment above line)
+// Providers.globalProvider = new MsalProvider({clientId: '{YOUR_CLIENT_ID}'});
 
 ReactDOM.render(<App />, document.getElementById('root'));
