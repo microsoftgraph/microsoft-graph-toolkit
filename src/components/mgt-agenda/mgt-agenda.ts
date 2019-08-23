4492b394fbe6ffc44c04dd8ca41b94eebc8b84e4
Nicolas Vogt (TEKsystems Inc) <nicolasjvogt@gmail.com>
2019-08-23 09:03:29 -0700

    [mgt-agenda] updating tsdocs

--- src/components/mgt-agenda/mgt-agenda.ts
+++ src/components/mgt-agenda/mgt-agenda.ts
@@ -17,24 +17,30 @@ import '../../styles/fabric-icon-font';
 import { MgtTemplatedComponent } from '../templatedComponent';
 import { prepScopes } from '../../Graph';
 import { MgtPersonDetails } from '../mgt-person/mgt-person';
-
+/**
+ * Web Component which represents events in a user or group calendar.
+ *
+ * @export
+ * @class MgtAgenda
+ * @extends {MgtTemplatedComponent}
+ */
 @customElement('mgt-agenda')
 export class MgtAgenda extends MgtTemplatedComponent {
   private _firstUpdated = false;
   /**
-   * _isNarrow property, determines width available for agenda component.
+   * determines width available for agenda component.
    * @type {boolean}
    */
   @property({ attribute: false }) private _isNarrow: boolean;
 
   /**
-   * _loading property, determines if agenda component is still loading details.
+   * determines if agenda component is still loading details.
    * @type {boolean}
    */
   @property({ attribute: false }) private _loading: boolean = true;
 
   /**
-   * events property, array containg events from user agenda.
+   * array containg events from user agenda.
    * @type {Array<MicrosoftGraph.Event>}
    */
   @property({
@@ -43,7 +49,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   events: Array<MicrosoftGraph.Event>;
 
   /**
-   * group-by-day property, allows developer to define agenda to group events by day.
+   * allows developer to define agenda to group events by day.
    * @type {Boolean}
    */
   @property({
@@ -54,7 +60,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   groupByDay = false;
 
   /**
-   * date property, stores current date for intial calender selection in events.
+   * stores current date for intial calender selection in events.
    * @type {string}
    */
   @property({
@@ -65,7 +71,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   date: string;
 
   /**
-   * days property, sets number of days until endate, 3 is the default
+   * sets number of days until endate, 3 is the default
    * @type {number}
    */
   @property({
@@ -76,7 +82,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   days: number = 3;
 
   /**
-   * event-query property, allows developer to define specific event
+   * allows developer to specify a different graph query that retrieves events
    * @type {string}
    */
   @property({
@@ -86,7 +92,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   eventQuery: string;
 
   /**
-   * show-max property, allows developer to define max number of events shown
+   * allows developer to define max number of events shown
    * @type {number}
    */
   @property({
@@ -95,7 +101,7 @@ export class MgtAgenda extends MgtTemplatedComponent {
   })
   showMax: number;
   /**
-   * group-id property, determines if agenda events come from specific group
+   * determines if agenda events come from specific group
    * @type {string}
    */
   @property({
@@ -113,7 +119,17 @@ export class MgtAgenda extends MgtTemplatedComponent {
     this.onResize = this.onResize.bind(this);
   }
 
-  firstUpdated() {
+  /**
+   * Invoked when the element is first updated. Implement to perform one time
+   * work on the element after update.
+   *
+   * Setting properties inside this method will trigger the element to update
+   * again after this update cycle completes.
+   *
+   * * @param _changedProperties Map of changed properties with old values
+   */
+
+  public firstUpdated() {
     this._firstUpdated = true;
     Providers.onProviderUpdated(() => this.loadData());
     this.loadData();
@@ -130,7 +146,16 @@ export class MgtAgenda extends MgtTemplatedComponent {
     super.disconnectedCallback();
   }
 
-  attributeChangedCallback(name, oldValue, newValue) {
+  /**
+   * Synchronizes property values when attributes change.
+   *
+   * @param {*} name
+   * @param {*} oldValue
+   * @param {*} newValue
+   * @memberof MgtAgenda
+   */
+
+  public attributeChangedCallback(name, oldValue, newValue) {
     if (oldValue !== newValue && (name === 'date' || name === 'days' || name === 'group-id')) {
       this.events = null;
       this.loadData();
