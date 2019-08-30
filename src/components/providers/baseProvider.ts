f3a937a539d426bd4e6c947c36788718ca083c08
Nikola Metulev <nmetulev@users.noreply.github.com>
2019-08-30 09:28:58 -0700

    TeamsProvider improvements and bug fixes (#132)
    * published 0.1.0
    
    * removed teams dependency and simplified teams sample
    
    * Setting up index for working with teams tabs
    
    * Fix for teams provider not closing the pop up when login in. Added loginhint to support single sign on
    
    * element providers now can depend on each other and only load when others are not available
    
    * minor cleanup
    
    * converted isAvailable from promise to regular getter
    
    * added comments and clean up - teams provider
    
    * applied workaround for acquireTokenSilent in TeamsProvider

--- src/components/providers/baseProvider.ts
+++ src/components/providers/baseProvider.ts
@@ -5,23 +5,61 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import { Providers } from '../../Providers';
+import { property } from 'lit-element';
+import { IProvider } from '../..';
 import { MgtBaseComponent } from '../baseComponent';
 
 export abstract class MgtBaseProvider extends MgtBaseComponent {
-  constructor() {
-    super();
-    Providers.onProviderUpdated(() => this.loadState());
-    this.loadState();
+  public get provider() {
+    return this._provider;
   }
 
-  private async loadState() {
-    const provider = Providers.globalProvider;
-
-    if (provider) {
-      provider.onStateChanged(() => {
-        this.fireCustomEvent('onStateChanged', provider.state);
-      });
+  public set provider(value: IProvider) {
+    if (this._provider) {
+      this.provider.removeStateChangedHandler(() => this.stateChangedHandler);
     }
+    this._provider = value;
+    if (this._provider) {
+      this.provider.onStateChanged(() => this.stateChangedHandler);
+    }
+  }
+
+  public abstract get isAvailable(): boolean;
+
+  @property({
+    attribute: 'depends-on',
+    type: String,
+    converter: newValue => {
+      return document.querySelector(newValue);
+    }
+  })
+  public dependsOn: MgtBaseProvider;
+
+  private _provider: IProvider;
+
+  protected firstUpdated(changedProperties) {
+    super.firstUpdated(changedProperties);
+
+    let higherPriority = false;
+    if (this.dependsOn) {
+      let higherPriorityProvider = this.dependsOn;
+      while (higherPriorityProvider) {
+        if (higherPriorityProvider.isAvailable) {
+          higherPriority = true;
+          break;
+        }
+        higherPriorityProvider = higherPriorityProvider.dependsOn;
+      }
+    }
+
+    if (!higherPriority && this.isAvailable) {
+      this.initializeProvider();
+    }
+  }
+
+  protected abstract initializeProvider();
+
+  private stateChangedHandler() {
+    this.fireCustomEvent('onStateChanged', this.provider.state);
   }
 }
--- src/providers/IProvider.ts
+++ src/providers/IProvider.ts
@@ -13,23 +13,23 @@ export abstract class IProvider implements AuthenticationProvider {
   private _state: ProviderState;
   private _loginChangedDispatcher = new EventDispatcher<LoginChangedEvent>();
 
-  get state(): ProviderState {
+  public get state(): ProviderState {
     return this._state;
   }
 
-  setState(state: ProviderState) {
-    if (state != this._state) {
+  public setState(state: ProviderState) {
+    if (state !== this._state) {
       this._state = state;
       this._loginChangedDispatcher.fire({});
     }
   }
 
   // events
-  onStateChanged(eventHandler: EventHandler<LoginChangedEvent>) {
+  public onStateChanged(eventHandler: EventHandler<LoginChangedEvent>) {
     this._loginChangedDispatcher.add(eventHandler);
   }
 
-  removeStateChangedHandler(eventHandler: EventHandler<LoginChangedEvent>) {
+  public removeStateChangedHandler(eventHandler: EventHandler<LoginChangedEvent>) {
     this._loginChangedDispatcher.remove(eventHandler);
   }
 
@@ -37,33 +37,33 @@ export abstract class IProvider implements AuthenticationProvider {
     this._state = ProviderState.Loading;
   }
 
-  login?(): Promise<void>;
-  logout?(): Promise<void>;
-  getAccessTokenForScopes(...scopes: string[]): Promise<string> {
-    return this.getAccessToken({ scopes: scopes });
+  public login?(): Promise<void>;
+  public logout?(): Promise<void>;
+  public getAccessTokenForScopes(...scopes: string[]): Promise<string> {
+    return this.getAccessToken({ scopes });
   }
 
-  abstract getAccessToken(options?: AuthenticationProviderOptions): Promise<string>;
-  graph: Graph;
+  public abstract getAccessToken(options?: AuthenticationProviderOptions): Promise<string>;
+  public graph: Graph;
 }
 
 export type EventHandler<E> = (event: E) => void;
 export interface LoginChangedEvent {}
 
 export class EventDispatcher<E> {
-  private eventHandlers: EventHandler<E>[] = [];
+  private eventHandlers: Array<EventHandler<E>> = [];
 
-  fire(event: E) {
-    for (let handler of this.eventHandlers) {
+  public fire(event: E) {
+    for (const handler of this.eventHandlers) {
       handler(event);
     }
   }
 
-  add(eventHandler: EventHandler<E>) {
+  public add(eventHandler: EventHandler<E>) {
     this.eventHandlers.push(eventHandler);
   }
 
-  remove(eventHandler: EventHandler<E>) {
+  public remove(eventHandler: EventHandler<E>) {
     for (let i = 0; i < this.eventHandlers.length; i++) {
       if (this.eventHandlers[i] === eventHandler) {
         this.eventHandlers.splice(i, 1);
