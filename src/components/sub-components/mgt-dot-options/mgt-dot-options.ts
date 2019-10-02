2b5b130cd64f8b33dcdff84631ab1e386abaff86
Nicolas Vogt (TEKsystems Inc) <nicolasjvogt@gmail.com>
2019-10-01 19:43:59 -0700

    [mgt-dot-options]

--- src/components/sub-components/mgt-dot-options/mgt-dot-options.ts
+++ src/components/sub-components/mgt-dot-options/mgt-dot-options.ts
@@ -9,15 +9,37 @@ import { customElement, html, property } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 import { MgtBaseComponent } from '../../baseComponent';
 import { styles } from './mgt-dot-options-css';
-
+/**
+ * Custom Component used to handle an arrow rendering for dressers utilized in the task component.
+ *
+ * @export MgtDotOptions
+ * @class MgtDotOptions
+ * @extends {MgtBaseComponent}
+ */
 @customElement('mgt-dot-options')
 export class MgtDotOptions extends MgtBaseComponent {
+
+  /**
+   * Array of styles to apply to the element. The styles should be defined
+   * user the `css` tag function.
+   */
   public static get styles() {
     return styles;
   }
+/**
+ * Determines if header menu is rendered or hidden.
+ *
+ * @type {boolean}
+ * @memberof MgtDotOptions
+ */
+@property({ type: Boolean }) public open: boolean = false;
 
-  @property({ type: Boolean }) public open: boolean = false;
-  @property({ type: Object }) public options: { [option: string]: (e: MouseEvent) => void | any } = null;
+/**
+ * Menu options to be rendered with an attached MouseEvent handler for expansion of details
+ *
+ * @memberof MgtDotOptions
+ */
+@property({ type: Object }) public options: { [option: string]: (e: MouseEvent) => void | any } = null;
 
   private _clickHandler: (e: MouseEvent) => void | any = null;
 
@@ -26,23 +48,24 @@ export class MgtDotOptions extends MgtBaseComponent {
     this._clickHandler = (e: MouseEvent) => (this.open = false);
   }
 
-  public connectedCallback() {
+// tslint:disable-next-line: completed-docs
+public connectedCallback() {
     super.connectedCallback();
     window.addEventListener('click', this._clickHandler);
   }
 
+// tslint:disable-next-line: completed-docs
   public disconnectedCallback() {
     window.removeEventListener('click', this._clickHandler);
     super.disconnectedCallback();
   }
 
-  private onDotClick(e: MouseEvent) {
-    e.preventDefault();
-    e.stopPropagation();
 
-    this.open = !this.open;
-  }
-
+/**
+ * Invoked on each update to perform rendering tasks. This method must return
+ * a lit-html TemplateResult. Setting properties inside this method will *not*
+ * trigger the element to update.
+ */
   public render() {
     return html`
       <div class=${classMap({ DotMenu: true, Open: this.open })} @click=${e => this.onDotClick(e)}>
@@ -53,8 +76,15 @@ export class MgtDotOptions extends MgtBaseComponent {
       </div>
     `;
   }
-
-  public getMenuOption(name: string, click: (e: MouseEvent) => void | any) {
+/**
+ * Used by the render method to attach click handler to each dot item
+ *
+ * @param {string} name
+ * @param {((e: MouseEvent) => void | any)} click
+ * @returns
+ * @memberof MgtDotOptions
+ */
+public getMenuOption(name: string, click: (e: MouseEvent) => void | any) {
     return html`
       <div
         class="DotItem"
@@ -70,5 +100,12 @@ export class MgtDotOptions extends MgtBaseComponent {
         </span>
       </div>
     `;
+  }
+
+  private onDotClick(e: MouseEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.open = !this.open;
   }
 }
