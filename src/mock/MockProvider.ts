e4ef01dc6f2f18e719c627171f94cb113b1ac9c0
Nicolas Vogt (TEKsystems Inc) <nicolasjvogt@gmail.com>
2019-10-02 19:21:12 -0700

    [MockProvider.ts]

--- src/mock/MockProvider.ts
+++ src/mock/MockProvider.ts
@@ -5,12 +5,27 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
-import { IProvider, ProviderState } from '../providers/IProvider';
-import { Graph } from '../Graph';
 import { Client } from '@microsoft/microsoft-graph-client';
-
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
+import { Graph } from '../Graph';
+import { IProvider, ProviderState } from '../providers/IProvider';
+/**
+ * Mock Provider access token for Microsoft Graph APIs
+ *
+ * @export
+ * @class MockProvider
+ * @extends {IProvider}
+ */
 export class MockProvider extends IProvider {
+  // tslint:disable-next-line: completed-docs
+  public provider: any;
+
+  /**
+   * new instance of mock graph provider
+   *
+   * @memberof MockProvider
+   */
+  public graph = new MockGraph(this);
   constructor(signedIn: boolean = false) {
     super();
     if (signedIn) {
@@ -20,26 +35,47 @@ export class MockProvider extends IProvider {
     }
   }
 
-  async login(): Promise<void> {
+  /**
+   * sets Provider state to SignedIn
+   *
+   * @returns {Promise<void>}
+   * @memberof MockProvider
+   */
+  public async login(): Promise<void> {
     this.setState(ProviderState.Loading);
     await new Promise(resolve => setTimeout(resolve, 3000));
     this.setState(ProviderState.SignedIn);
   }
-
-  async logout(): Promise<void> {
+  /**
+   * sets Provider state to signed out
+   *
+   * @returns {Promise<void>}
+   * @memberof MockProvider
+   */
+  public async logout(): Promise<void> {
     this.setState(ProviderState.Loading);
     await new Promise(resolve => setTimeout(resolve, 3000));
     this.setState(ProviderState.SignedOut);
   }
-
-  getAccessToken(): Promise<string> {
+  /**
+   * Promise returning token from graph.microsoft.com
+   *
+   * @returns {Promise<string>}
+   * @memberof MockProvider
+   */
+  public getAccessToken(): Promise<string> {
     return Promise.resolve('{token:https://graph.microsoft.com/}');
   }
-  provider: any;
-
-  graph = new MockGraph(this);
 }
 
+/**
+ * MockGraph Instance
+ *
+ * @export
+ * @class MockGraph
+ * @extends {Graph}
+ */
+// tslint:disable-next-line: max-classes-per-file
 export class MockGraph extends Graph {
   private baseUrl = 'https://proxy.apisandbox.msdn.microsoft.com/svc?url=';
   private rootGraphUrl: string = 'https://graph.microsoft.com/';
@@ -48,17 +84,24 @@ export class MockGraph extends Graph {
     super(null);
 
     this.client = Client.initWithMiddleware({
-      baseUrl: this.baseUrl + escape(this.rootGraphUrl),
-      authProvider: provider
+      authProvider: provider,
+      baseUrl: this.baseUrl + escape(this.rootGraphUrl)
     });
   }
+/**
+ * get events for Calendar
+ *
+ * @param {Date} startDateTime
+ * @param {Date} endDateTime
+ * @returns {Promise<MicrosoftGraph.Event[]>}
+ * @memberof MockGraph
+ */
+public async getEvents(startDateTime: Date, endDateTime: Date): Promise<MicrosoftGraph.Event[]> {
+    const sdt = `startdatetime=${startDateTime.toISOString()}`;
+    const edt = `enddatetime=${endDateTime.toISOString()}`;
+    const uri = `/me/calendarview?${sdt}&${edt}`;
 
-  async getEvents(startDateTime: Date, endDateTime: Date): Promise<Array<MicrosoftGraph.Event>> {
-    let sdt = `startdatetime=${startDateTime.toISOString()}`;
-    let edt = `enddatetime=${endDateTime.toISOString()}`;
-    let uri = `/me/calendarview?${sdt}&${edt}`;
-
-    let calendarView = await this.client.api(escape(uri)).get();
+    const calendarView = await this.client.api(escape(uri)).get();
     return calendarView ? calendarView.value : null;
   }
 }
