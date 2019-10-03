e79fb9922ba933b04514f0508d03efd16ac5b6b2
Nicolas Vogt (TEKsystems Inc) <nicolasjvogt@gmail.com>
2019-10-03 09:29:04 -0700

    [index.es5.ts] alphabetical

--- src/bundle/index.es5.ts
+++ src/bundle/index.es5.ts
@@ -5,7 +5,7 @@
  * -------------------------------------------------------------------------------------------
  */
 
+import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
 import 'babel-polyfill';
 import 'whatwg-fetch';
-import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
 export * from './index.es6';
