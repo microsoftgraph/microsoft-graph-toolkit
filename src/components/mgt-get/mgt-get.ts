c458386f218097b09512a61c1841fc93a056fbd1
Nikola Metulev <nikolame@microsoft.com>
2019-10-18 09:34:34 -0700

    added license header

--- src/components/mgt-get/mgt-get.ts
+++ src/components/mgt-get/mgt-get.ts
@@ -1,4 +1,11 @@
-import { css, customElement, html, property } from 'lit-element';
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
+import { customElement, property } from 'lit-element';
 import { Providers } from '../../Providers';
 import { ProviderState } from '../../providers/IProvider';
 import { prepScopes } from '../../utils/GraphHelpers';
