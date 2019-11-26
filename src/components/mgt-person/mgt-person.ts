fc0d7a7334baa14e2ed36c411a54ae4f1b00580c
Nicolas Vogt <nicolasjvogt@gmail.com>
2019-11-26 09:13:10 -0800

    [person] updating mouseEnter to not reset if click inside of the card (#209)
    * [person] updating mouseEnter to not reset if click inside of the card
    
    * [person] fixing hover showing person-card during click state

--- src/components/mgt-person/mgt-person.ts
+++ src/components/mgt-person/mgt-person.ts
@@ -293,12 +293,11 @@ export class MgtPerson extends MgtTemplatedComponent {
   }
 
   private _handleMouseEnter(e: MouseEvent) {
+    clearTimeout(this._mouseEnterTimeout);
+    clearTimeout(this._mouseLeaveTimeout);
     if (this.personCardInteraction !== PersonCardInteraction.hover) {
       return;
     }
-
-    clearTimeout(this._mouseEnterTimeout);
-    clearTimeout(this._mouseLeaveTimeout);
     this._mouseEnterTimeout = setTimeout(this._showPersonCard.bind(this), 500);
   }
 
