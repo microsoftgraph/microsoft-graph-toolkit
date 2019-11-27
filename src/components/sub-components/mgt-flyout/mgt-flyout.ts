65b0c04145b35f765b0885a10385ba36238af26e
Nikola Metulev <nikolame@microsoft.com>
2019-11-27 09:54:09 -0800

    added license header

--- src/components/sub-components/mgt-flyout/mgt-flyout.ts
+++ src/components/sub-components/mgt-flyout/mgt-flyout.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 import { customElement, html, LitElement, property, PropertyValues } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map';
 import { styles } from './mgt-flyout-css';
