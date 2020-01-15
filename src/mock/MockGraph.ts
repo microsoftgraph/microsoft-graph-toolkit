cee94bb23a6818a00526463c98ef4f239e50823d
Shane Weaver <shweaver@microsoft.com>
2020-01-14 16:48:29 -0800

    Adding license headers to new files

--- src/mock/MockGraph.ts
+++ src/mock/MockGraph.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 import { Client } from '@microsoft/microsoft-graph-client';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import { BaseGraph, IGraph } from '../BaseGraph';
