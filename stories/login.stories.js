c8af69451daa92f37323a09c4f147eb2db189dc5
beth-panx <beth_panx@hotmail.com>
2020-02-12 11:00:47 -0800

    Updated folder structure to accomendate future addons

--- stories/login.stories.js
+++ stories/login.stories.js
@@ -9,8 +9,8 @@ import { html } from 'lit-element';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withWebComponentsKnobs } from 'storybook-addon-web-components-knobs';
-import { withSignIn } from '../.storybook/signInAddon';
-import { withCodeEditor } from '../.storybook/codeEditorAddon/codeAddon';
+import { withSignIn } from '../.storybook/addons/signInAddon/signInAddon';
+import { withCodeEditor } from '../.storybook/addons/codeEditorAddon/codeAddon';
 import '../dist/es6/components/mgt-login/mgt-login';
 
 export default {
