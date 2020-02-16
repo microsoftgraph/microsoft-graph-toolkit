f3a20b6eea26b4ecd344073160b0ccdd7f99127b
Herrick Spencer <herrick.spencer@microsoft.com>
2020-02-15 22:34:28 -0800

    Storybook: Add person card examples

--- stories/person.stories.js
+++ stories/person.stories.js
@@ -23,3 +23,32 @@ export default {
 export const person = () => html`
   <mgt-person person-query="me" show-name show-email></mgt-person>
 `;
+
+export const personCard_hover = () => html`
+  <mgt-person person-query="me" person-card="hover"></mgt-person>
+`;
+
+export const personCard_inheritDetails = () => html`
+  <mgt-person person-query="me" show-name show-email person-card="hover">
+    <template data-type="person-card">
+      <mgt-person-card inherit-details></mgt-person-card>
+    </template>
+  </mgt-person>
+`;
+
+export const personCard_additionalDetails = () => html`
+  <mgt-person person-query="me" show-name show-email person-card="hover">
+    <template data-type="person-card">
+      <mgt-person-card inherit-details>
+        <template data-type="additional-details">
+          <h3>Stuffed Animal Friends:</h3>
+          <ul>
+            <li>Giraffe</li>
+            <li>lion</li>
+            <li>Rabbit</li>
+          </ul>
+        </template>
+      </mgt-person-card>
+    </template>
+  </mgt-person>
+`;
