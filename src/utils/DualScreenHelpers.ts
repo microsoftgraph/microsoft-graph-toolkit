f896469b329fbb7ec873b8ea9f41fd38f13f3a28
Shane Weaver <shweaver@microsoft.com>
2020-02-26 15:39:58 -0800

    Adding license header

--- src/utils/DualScreenHelpers.ts
+++ src/utils/DualScreenHelpers.ts
@@ -1,4 +1,11 @@
 /**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
+/**
  * An object containing width, height, top and left properties (aka segment's bounding rects)
  *
  * @interface IWindowSegment
