163ba95b91d867acbfbae11090edb7153429a298
Shane Weaver <shweaver@microsoft.com>
2020-03-04 10:20:31 -0800

    Adding agenda-extensions example

--- src/components/mgt-tasks/mgt-tasks.ts
+++ src/components/mgt-tasks/mgt-tasks.ts
@@ -1155,7 +1155,7 @@ export class MgtTasks extends MgtTemplatedComponent {
       : [];
 
     const noPeopleTemplate = html`
-      <template data-type="error">
+      <template data-type="no-data">
         <i class="login-icon ms-Icon ms-Icon--Contact"></i>
       </template>
     `;
