7d94efa5e829ca7746f68a7d290d968f062c92f7
Nicolas Vogt <nicolasjvogt@gmail.com>
2020-04-28 14:27:55 -0700

    people-picker ui update and dark mode story

--- stories/components/peoplePicker.stories.js
+++ stories/components/peoplePicker.stories.js
@@ -25,5 +25,26 @@ export const peoplePicker = () => html`
 `;
 
 export const groupId = () => html`
-<mgt-people-picker group-id="02bd9fd6-8f93-4758-87c3-1fb73740a315"></mgt-people-picker>
+  <mgt-people-picker group-id="02bd9fd6-8f93-4758-87c3-1fb73740a315"></mgt-people-picker>
+`;
+
+export const DarkMode = () => html`
+  <mgt-people-picker></mgt-people-picker>
+  <style>
+    .story-mgt-preview-wrapper {
+      background-color: black;
+    }
+
+    mgt-people-picker {
+      --input-border: 2px rgba(255, 255, 255, 0.5) solid;
+      --input-background-color: #1f1f1f;
+      --dropdown-background-color: #1f1f1f;
+      --dropdown-item-hover-background: #333d47;
+      --dropdown-item-selected-background: #0f78d4;
+      --input-hover-color: #008394;
+      --input-focus-color: #0f78d4;
+      --font-color: white;
+      --placeholder-focus-color: rgba(255, 255, 255, 0.8);
+    }
+  </style>
 `;
