72c2c9969c324697307b790e8e0aa9c3b95dfd0d
Nikola Metulev <nmetulev@users.noreply.github.com>
2020-05-13 10:41:26 -0700

    adding dotnetcore sample (#379)
    Co-authored-by: Shane Weaver <shweaver@microsoft.com>

---
+++ samples/proxy-provider-asp-net-core/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MicrosoftGraphAspNetCoreConnectSample.Helpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.AspNetCore.Hosting;
+using System.Security.Claims;
+
+namespace MicrosoftGraphAspNetCoreConnectSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _env;
+        private readonly IGraphSdkHelper _graphSdkHelper;
+
+        public HomeController(IConfiguration configuration, IHostingEnvironment hostingEnvironment, IGraphSdkHelper graphSdkHelper)
+        {
+            _configuration = configuration;
+            _env = hostingEnvironment;
+            _graphSdkHelper = graphSdkHelper;
+        }
+
+        [AllowAnonymous]
+        // Load user's profile.
+        public async Task<IActionResult> Index(string email)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Get users's email.
+                email = email ?? User.FindFirst("preferred_username")?.Value;
+                ViewData["Email"] = email;
+
+                // Initialize the GraphServiceClient.
+                var graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+
+                ViewData["Response"] = await GraphService.GetUserJson(graphClient, email, HttpContext);
+
+                ViewData["Picture"] = await GraphService.GetPictureBase64(graphClient, email, HttpContext);
+            }
+
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        // Send an email message from the current user.
+        public async Task<IActionResult> SendEmail(string recipients)
+        {
+            if (string.IsNullOrEmpty(recipients))
+            {
+                TempData["Message"] = "Please add a valid email address to the recipients list!";
+                return RedirectToAction("Index");
+            }
+
+            try
+            {
+                // Initialize the GraphServiceClient.
+                var graphClient = _graphSdkHelper.GetAuthenticatedClient((ClaimsIdentity)User.Identity);
+
+                // Send the email.
+                await GraphService.SendEmail(graphClient, _env, recipients, HttpContext);
+
+                // Reset the current user's email address and the status to display when the page reloads.
+                TempData["Message"] = "Success! Your mail was sent.";
+                return RedirectToAction("Index");
+            }
+            catch (ServiceException se)
+            {
+                if (se.Error.Code == "Caller needs to authenticate.") return new EmptyResult();
+                return RedirectToAction("Error", "Home", new { message = "Error: " + se.Error.Message });
+            }
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ samples/proxy-provider-asp-net-core/Helpers/GraphAuthProvider.cs
@@ -0,0 +1,81 @@
+﻿/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using MicrosoftGraphAspNetCoreConnectSample.Extensions;
+
+namespace MicrosoftGraphAspNetCoreConnectSample.Helpers
+{
+    public class GraphAuthProvider : IGraphAuthProvider
+    {
+        private IConfidentialClientApplication _app;
+        private readonly string[] _scopes;
+
+        public GraphAuthProvider(IConfiguration configuration)
+        {
+            var azureOptions = new AzureAdOptions();
+            configuration.Bind("AzureAd", azureOptions);
+
+            // More info about MSAL Client Applications: https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/Client-Applications
+            _app = ConfidentialClientApplicationBuilder.Create(azureOptions.ClientId)
+                    .WithClientSecret(azureOptions.ClientSecret)
+                    .WithAuthority(AzureCloudInstance.AzurePublic, AadAuthorityAudience.AzureAdAndPersonalMicrosoftAccount)
+                    .WithRedirectUri(azureOptions.BaseUrl + azureOptions.CallbackPath)
+                    .Build();
+
+            Authority = _app.Authority;
+
+            _scopes = azureOptions.GraphScopes.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public string Authority { get; }
+
+        // Gets an access token. First tries to get the access token from the token cache.
+        // Using password (secret) to authenticate. Production apps should use a certificate.
+        public async Task<string> GetUserAccessTokenAsync(string userId)
+        {
+            var account = await _app.GetAccountAsync(userId);
+            if (account == null) throw new ServiceException(new Error
+            {
+                Code = "TokenNotFound",
+                Message = "User not found in token cache. Maybe the server was restarted."
+            });
+
+            try
+            {
+                var result = await _app.AcquireTokenSilent(_scopes, account).ExecuteAsync();
+                return result.AccessToken;
+            }
+
+            // Unable to retrieve the access token silently.
+            catch (Exception)
+            {
+                throw new ServiceException(new Error
+                {
+                    Code = GraphErrorCode.AuthenticationFailure.ToString(),
+                    Message = "Caller needs to authenticate. Unable to retrieve the access token silently."
+                });
+            }
+        }
+
+        public async Task<AuthenticationResult> GetUserAccessTokenByAuthorizationCode(string authorizationCode)
+        {
+            return await _app.AcquireTokenByAuthorizationCode(_scopes, authorizationCode).ExecuteAsync();
+        }
+    }
+
+    public interface IGraphAuthProvider
+    {
+        string Authority { get; }
+
+        Task<string> GetUserAccessTokenAsync(string userId);
+
+        Task<AuthenticationResult> GetUserAccessTokenByAuthorizationCode(string authorizationCode);
+    }
+}
