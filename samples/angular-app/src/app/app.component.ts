51945a71f8bef8e049fb51910002e19d71d76a37
Cameron Dwyer <CameronDwyer@users.noreply.github.com>
2020-07-16 05:30:06 +1000

    New sample Angular app (#551)
    * New Angular app sample
    
    * Removed Twitter link

--- samples/angular-app/src/app/app.component.ts
+++ samples/angular-app/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import {MgtPersonDetails} from '@microsoft/mgt';
+import { Component, OnInit } from '@angular/core';
+import { Providers, MsalProvider, TemplateHelper, ProviderState} from '@microsoft/mgt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  person : MgtPersonDetails = {
-    displayName: "Nikola Metulev"
-  };
+export class AppComponent implements OnInit {
+  title = 'demo-mgt-angular';
 
-  loginCompleted = e => {
-    console.log('loginCompleted');
+  public isLoggedIn(){
+    return Providers.globalProvider.state === ProviderState.SignedIn;
+  }
+
+  ngOnInit()
+  {
+    Providers.globalProvider = new MsalProvider({ clientId: '[YOUR-CLIENT-ID]' });
+    TemplateHelper.setBindingSyntax('[[', ']]');
   }
 }
