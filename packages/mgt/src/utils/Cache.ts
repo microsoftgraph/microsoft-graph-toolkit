cea86f33dc4ca15e858d74675d80d755c5f69ff5
adrien-chaussabel <adrien.chaussabel@gmail.com>
2020-08-03 12:54:37 -0700

    added MS header

--- packages/mgt/src/utils/Cache.ts
+++ packages/mgt/src/utils/Cache.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 // TODO ands consideration
 // Should db clear on sign out
 // SHould db be named with the user id
