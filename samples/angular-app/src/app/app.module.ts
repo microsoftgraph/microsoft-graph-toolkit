74df0c4ccf26924f8fee28b8e051402fca4cfd46
Matthew Haugen <mhaugen@haugenapplications.com>
2020-08-11 13:05:36 -0700

    Use MsalModule.forRoot instead of injection tokens

--- samples/angular-app/src/app/app.module.ts
+++ samples/angular-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { MsalModule, MsalService, MSAL_CONFIG, MSAL_CONFIG_ANGULAR } from '@azure/msal-angular';
+import { MsalModule } from '@azure/msal-angular';
 import { MSALAngularConfig as MsalAngularConfig, MsalConfig } from 'src/environments/environment.msal';
 import { AngularAgendaComponent } from './angular-agenda/angular-agenda.component';
 import { AppComponent } from './app.component';
@@ -16,19 +16,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    MsalModule,
+    MsalModule.forRoot(MsalConfig, MsalAngularConfig),
     RouterModule.forRoot([]),
-  ],
-  providers: [
-    MsalService,
-    {
-      provide: MSAL_CONFIG,
-      useValue: MsalConfig,
-    },
-    {
-      provide: MSAL_CONFIG_ANGULAR,
-      useValue: MsalAngularConfig,
-    },
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
