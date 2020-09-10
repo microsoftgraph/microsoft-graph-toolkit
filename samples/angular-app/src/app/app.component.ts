f36560fff5e4498a6431ad30e4175c6146e09f19
Shane Weaver <shweaver@microsoft.com>
2020-09-10 11:33:09 -0700

    Integrated angular app with locally built packages

--- samples/angular-app/src/app/app.component.ts
+++ samples/angular-app/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
-import { LoginType, MsalProvider, Providers, ProviderState, TemplateHelper } from '@microsoft/mgt';
-import { MSALAngularConfig } from 'src/environments/environment.msal';
+import { MsalProvider } from '@microsoft/mgt';
+import { LoginType, Providers, ProviderState, TemplateHelper } from '@microsoft/mgt-element';
+import { MSALAngularConfig } from '../environments/environment.msal';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
     Providers.globalProvider = new MsalProvider({
       userAgentApplication: msalService,
       scopes: MSALAngularConfig.consentScopes,
-      loginType: MSALAngularConfig.popUp === true ? LoginType.Popup : LoginType.Redirect,
+      loginType: MSALAngularConfig.popUp === true ? LoginType.Popup : LoginType.Redirect
     });
 
     TemplateHelper.setBindingSyntax('[[', ']]');
@@ -25,6 +26,5 @@ export class AppComponent implements OnInit {
     return Providers.globalProvider.state === ProviderState.SignedIn;
   }
 
-  public ngOnInit() {
-  }
+  public ngOnInit() {}
 }
--- samples/angular-app/src/app/app.module.ts
+++ samples/angular-app/src/app/app.module.ts
@@ -2,24 +2,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { MsalModule } from '@azure/msal-angular';
-import { MSALAngularConfig as MsalAngularConfig, MsalConfig } from 'src/environments/environment.msal';
+import { MSALAngularConfig as MsalAngularConfig, MsalConfig } from '../environments/environment.msal';
 import { AngularAgendaComponent } from './angular-agenda/angular-agenda.component';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    AngularAgendaComponent
-  ],
-  imports: [
-    BrowserModule,
-    MsalModule.forRoot(MsalConfig, MsalAngularConfig),
-    RouterModule.forRoot([]),
-  ],
+  declarations: [AppComponent, NavBarComponent, AngularAgendaComponent],
+  imports: [BrowserModule, MsalModule.forRoot(MsalConfig, MsalAngularConfig), RouterModule.forRoot([])],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
