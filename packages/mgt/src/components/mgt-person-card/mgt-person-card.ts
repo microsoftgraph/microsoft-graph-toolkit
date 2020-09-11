f76a0d05812371fe673344b954d1eb87697eb109
Shane Weaver <shweaver@microsoft.com>
2020-09-11 15:23:31 -0700

    Using SvgHelper in person card

--- packages/mgt/src/components/mgt-person-card/mgt-person-card.ts
+++ packages/mgt/src/components/mgt-person-card/mgt-person-card.ts
@@ -24,6 +24,7 @@ import { MgtPersonCardOrganization } from './sections/mgt-person-card-organizati
 import { MgtPersonCardProfile } from './sections/mgt-person-card-profile/mgt-person-card-profile';
 import { Presence } from '@microsoft/microsoft-graph-types-beta';
 import { getUserPresence } from '../../graph/graph.presence';
+import { getSvg, SvgIcon } from '../../utils/SvgHelper';
 
 /**
  * Web Component used to show detailed data for a person in the Microsoft Graph
@@ -453,13 +454,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     if (getEmailFromGraphEntity(person)) {
       email = html`
         <div class="icon" @click=${() => this.emailUser()}>
-          <svg width="14" height="10" viewBox="0 0 14 10" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M11.8473 1H2.04886L6.47364 4.18916C6.64273 4.31103 6.86969 4.31522 7.04316 4.19968L11.8473 1ZM1 1.47671V9H13V1.43376L7.59749 5.03198C7.07706 5.3786 6.39621 5.36601 5.88894 5.0004L1 1.47671ZM0 1C0 0.447715 0.447715 0 1 0H13C13.5523 0 14 0.447715 14 1V9C14 9.55228 13.5523 10 13 10H1C0.447716 10 0 9.55229 0 9V1Z"
-            />
-          </svg>
+          ${getSvg(SvgIcon.SmallEmail)}
           <span>Send email</span>
         </div>
       `;
@@ -470,13 +465,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     if (userPerson.userPrincipalName) {
       chat = html`
         <div class="icon" @click=${() => this.chatUser()}>
-          <svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M9.5781 8.26403L9.57811 8.26403C9.68611 8.29867 9.8455 8.36364 10.0125 8.50479C10.1405 8.61294 10.2409 8.73883 10.3235 8.86465L11.9634 10.9944C11.9701 10.9924 11.9753 10.9904 11.9785 10.9889C11.9841 10.9864 11.9918 10.9823 12 10.9768V1.32284C12 1.18078 11.8731 1 11.6207 1H1.37926C1.12692 1 1 1.18078 1 1.32284V7.37377C1 7.45357 1.01415 7.49036 1.02102 7.50507C1.02778 7.51955 1.04342 7.54689 1.09159 7.58705L1.10485 7.5981L1.11771 7.6096C1.13526 7.62529 1.21707 7.69076 1.33937 7.76027C1.46122 7.82952 1.58119 7.87864 1.67944 7.89966L1.69102 7.90214L1.691 7.9022C3.32106 8.27116 6.2626 8.27688 8.67896 8.18036L8.69908 8.17955L8.71921 8.17956L8.7627 8.17954C9.01362 8.17932 9.31313 8.17907 9.5781 8.26403ZM11.2376 11.6908L9.50506 9.44081C9.39493 9.26422 9.32445 9.23285 9.27276 9.21627C9.17534 9.18504 9.0401 9.17966 8.71888 9.17956C6.31879 9.27543 3.24831 9.27999 1.47024 8.87753C1.01314 8.77974 0.600449 8.48852 0.451238 8.35513C0.15593 8.10893 0 7.78958 0 7.37377V1.32284C0 0.593147 0.610664 0 1.37926 0H11.6207C12.3893 0 13 0.593147 13 1.32284V11.0441C13 11.4686 12.6828 11.7689 12.3881 11.9012C12.1048 12.0284 11.673 12.0728 11.3387 11.7993C11.3003 11.7679 11.2678 11.7301 11.2376 11.6908ZM3 3.5C3 3.22386 3.22386 3 3.5 3H9.5C9.77614 3 10 3.22386 10 3.5C10 3.77614 9.77614 4 9.5 4H3.5C3.22386 4 3 3.77614 3 3.5ZM3.5 5C3.22386 5 3 5.22386 3 5.5C3 5.77614 3.22386 6 3.5 6H6.5C6.77614 6 7 5.77614 7 5.5C7 5.22386 6.77614 5 6.5 5H3.5Z"
-            />
-          </svg>
+          ${getSvg(SvgIcon.SmallChat)}
           <span>Start chat</span>
         </div>
       `;
@@ -497,9 +486,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
   protected renderExpandedDetailsButton(): TemplateResult {
     return html`
       <div class="expanded-details-button" @click=${() => this.showExpandedDetails()}>
-        <svg width="15" height="8" viewBox="0 0 15 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M14 1L7.5 7L1 1" stroke="#3078CD" />
-        </svg>
+        ${getSvg(SvgIcon.ExpandDown)}
       </div>
     `;
   }
@@ -556,11 +543,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     });
     return html`
       <button class=${overviewClasses} @click=${() => this.updateCurrentSection(null)}>
-        <svg xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M12 4V9H2V4H12ZM11 8V5H3V8H11ZM13 4H18V9H13V4ZM17 8V5H14V8H17ZM8 15V10H18V15H8ZM9 11V14H17V11H9ZM2 15V10H7V15H2ZM3 11V14H6V11H3Z"
-          />
-        </svg>
+        ${getSvg(SvgIcon.Overview)}
       </button>
       ${navIcons}
     `;
@@ -598,11 +581,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
           }}
         />
         <button class="quick-message__send" @click=${() => this.sendQuickMessage()}>
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M4.27144 8.99999L1.72572 2.45387C1.54854 1.99826 1.9928 1.56256 2.43227 1.71743L2.50153 1.74688L16.0015 8.49688C16.3902 8.69122 16.4145 9.22336 16.0744 9.45992L16.0015 9.50311L2.50153 16.2531C2.0643 16.4717 1.58932 16.0697 1.70282 15.6178L1.72572 15.5461L4.27144 8.99999L1.72572 2.45387L4.27144 8.99999ZM3.3028 3.4053L5.25954 8.43705L10.2302 8.43749C10.515 8.43749 10.7503 8.64911 10.7876 8.92367L10.7927 8.99999C10.7927 9.28476 10.5811 9.52011 10.3065 9.55736L10.2302 9.56249L5.25954 9.56205L3.3028 14.5947L14.4922 8.99999L3.3028 3.4053Z"
-            />
-          </svg>
+          ${getSvg(SvgIcon.Send)}
         </button>
       </div>
       <div class="sections">
