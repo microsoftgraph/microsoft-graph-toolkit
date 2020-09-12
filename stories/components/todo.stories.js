165072b9014cb8078b9e89a2a27dbe295f263e4c
Shane Weaver <shweaver@microsoft.com>
2020-09-12 16:59:08 -0700

    Added todo docs and story. Also a few css fixes (#623)

---
+++ stories/components/todo.stories.js
@@ -0,0 +1,23 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
+import { html } from 'lit-element';
+import { withA11y } from '@storybook/addon-a11y';
+import { withSignIn } from '../../.storybook/addons/signInAddon/signInAddon';
+import { withCodeEditor } from '../../.storybook/addons/codeEditorAddon/codeAddon';
+import '../../packages/mgt/dist/es6/components/mgt-todo/mgt-todo';
+
+export default {
+  title: 'Components | mgt-todo',
+  component: 'mgt-todo',
+  decorators: [withA11y, withSignIn, withCodeEditor],
+  parameters: { options: { selectedPanel: 'storybookjs/knobs/panel' } }
+};
+
+export const tasks = () => html`
+  <mgt-todo></mgt-todo>
+`;
