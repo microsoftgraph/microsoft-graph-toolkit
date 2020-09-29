ebf1ca62935f84d32dec223c66b31c8062551ae9
Nicolas Vogt <nicolasjvogt@gmail.com>
2020-09-29 10:34:56 -0700

    updated with common enums for easier selection of localized strings

--- packages/mgt/src/utils/localizationHelper.ts
+++ packages/mgt/src/utils/localizationHelper.ts
@@ -29,6 +29,15 @@ export class LocalizationHelper extends LitElement {
     return document.body.getAttribute('dir');
   }
 
+  /**
+   * Recieves string and associated tagname from component, compares to new strings
+   *
+   * @static
+   * @param {*} tagName
+   * @param {*} stringKey
+   * @returns
+   * @memberof LocalizationHelper
+   */
   public static getString(tagName, stringKey) {
     if (this._strings) {
       let newStringKeys = Object.keys(this._strings);
@@ -37,18 +46,68 @@ export class LocalizationHelper extends LitElement {
         return stringKey;
       }
 
+      //converts strings to enum
+      function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key] === value);
+      }
+
+      let enumKey = getKeyByValue(StoredString, stringKey);
+
       for (let tagKey of newStringKeys) {
         //match tagName
         if (tagKey.toLowerCase() === tagName.toLowerCase()) {
           //checks if tagName has user string
-          if (!this._strings[tagName.toLowerCase()][stringKey]) {
+          if (!this._strings[tagName.toLowerCase()][enumKey]) {
             return stringKey;
           }
 
           //match string
-          return this._strings[tagName.toLowerCase()][stringKey];
+          return this._strings[tagName.toLowerCase()][enumKey];
         }
       }
     }
   }
+}
+
+/**
+ * StoredString
+ *
+ * @export
+ * @enum {number}
+ */
+export enum StoredString {
+  /**
+   * No results found for a picker
+   */
+  noResultsFound = "We didn't find any matches.",
+
+  /**
+   * No results found for a picker
+   */
+  loading = 'Loading...',
+
+  /**
+   * placeholder for mgt-people-picker
+   */
+  peoplePickerPlaceholder = 'Start typing a name',
+
+  /**
+   * Sign in for mgt-login
+   */
+  signIn = 'Sign In',
+
+  /**
+   * Sign out for mgt-login
+   */
+  signOut = 'Sign Out',
+
+  /**
+   * placeholder for mgt-teams-channel-picker
+   */
+  channelPickerPlaceholder = 'Select a channel',
+
+  /**
+   * placeholder for mgt-tasks new task
+   */
+  newTaskPlaceholder = 'Task...'
 }
