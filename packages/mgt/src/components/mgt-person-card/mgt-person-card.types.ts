098b4321baf39726040e485d735054edefd953f1
Nikola Metulev <nikolame@microsoft.com>
2020-11-18 21:18:17 -0800

    added license

--- packages/mgt/src/components/mgt-person-card/mgt-person-card.types.ts
+++ packages/mgt/src/components/mgt-person-card/mgt-person-card.types.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 import { Message, Person, SharedInsight, User } from '@microsoft/microsoft-graph-types';
 import { Profile } from '@microsoft/microsoft-graph-types-beta';
 
