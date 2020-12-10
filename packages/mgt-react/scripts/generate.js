d26648c03831ec6394abcf187217c7aca32dbefe
Nikola Metulev <nikolame@microsoft.com>
2020-12-09 22:46:04 -0800

    Exported mgt-components and mgt-element types

--- packages/mgt-react/scripts/generate.js
+++ packages/mgt-react/scripts/generate.js
@@ -3,7 +3,8 @@ var fs = require('fs-extra');
 let wc = JSON.parse(fs.readFileSync(`${__dirname}/../temp/web-components.json`));
 
 const primitives = new Set(['string', 'boolean', 'number', 'any']);
-const mgtImports = new Set();
+const mgtComponentImports = new Set();
+const mgtElementImports = new Set();
 
 const tags = new Set([
   'mgt-person',
@@ -55,14 +56,15 @@ for (const tag of wc.tags) {
 
       if (
         type.startsWith('MicrosoftGraph.') ||
-        type.startsWith('MicrosoftGraphBeta.') ||
-        type.startsWith('MgtElement.')
+        type.startsWith('MicrosoftGraphBeta.')
       ) {
         continue;
       }
 
-      if (!primitives.has(type) && !mgtImports.has(type)) {
-        mgtImports.add(type);
+      if (type.startsWith('MgtElement.') && !mgtElementImports.has(type)) {
+        mgtElementImports.add(type.split('.')[1]);
+      } else if (!primitives.has(type) && !mgtComponentImports.has(type)) {
+        mgtComponentImports.add(type);
       }
     }
   }
@@ -70,7 +72,11 @@ for (const tag of wc.tags) {
   let propsType = '';
 
   for (const prop in props) {
-    propsType += `\t${prop}?: ${props[prop]};\n`;
+    let type = props[prop];
+    if (type.startsWith('MgtElement.')) {
+      type = type.split('.')[1];
+    }
+    propsType += `\t${prop}?: ${type};\n`;
   }
 
   if (tag.events) {
@@ -86,12 +92,14 @@ for (const wrapper of wrappers) {
   output += `\nexport const ${wrapper.className} = wrapMgt<${wrapper.propsType}>('${wrapper.tag}');\n`;
 }
 
-output = `import { ${Array.from(mgtImports).join(',')} } from '@microsoft/mgt-components';
-import * as MgtElement from '@microsoft/mgt-element';
+output = `import { ${Array.from(mgtComponentImports).join(',')} } from '@microsoft/mgt-components';
+import { ${Array.from(mgtElementImports).join(',')} } from '@microsoft/mgt-element';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import * as MicrosoftGraphBeta from '@microsoft/microsoft-graph-types-beta';
 import {wrapMgt} from '../Mgt';
 ${output}
+export { ${Array.from(mgtComponentImports).join(',')} } from '@microsoft/mgt-components';
+export { ${Array.from(mgtElementImports).join(',')} } from '@microsoft/mgt-element';
 `;
 
 if (!fs.existsSync(`${__dirname}/../src/generated`)) {
