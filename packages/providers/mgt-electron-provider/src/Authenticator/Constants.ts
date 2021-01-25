49ed82e124f1b75237ea2c89d8ef19389ce78cf7
Nikola Metulev <nikolame@microsoft.com>
2021-01-25 08:49:32 -0800

    Added license

--- packages/providers/mgt-electron-provider/src/Authenticator/Constants.ts
+++ packages/providers/mgt-electron-provider/src/Authenticator/Constants.ts
@@ -1,2 +1,9 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 export const CACHE_LOCATION = './data/cache.json';
 export const REDIRECT_URI = 'msal://redirect';
--- packages/providers/mgt-electron-provider/src/Authenticator/CustomFileProtocol.ts
+++ packages/providers/mgt-electron-provider/src/Authenticator/CustomFileProtocol.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
