fcc3b3efa42dcd51f9fe9f663b1ff1af69e97153
Nikola Metulev <nmetulev@users.noreply.github.com>
2021-02-01 08:33:34 -0800

    Update stories/components/peoplePicker.stories.js

--- stories/components/peoplePicker.stories.js
+++ stories/components/peoplePicker.stories.js
@@ -142,7 +142,6 @@ export const pickerDisabled = () => html`
   <mgt-people-picker
     default-selected-user-ids="e3d0513b-449e-4198-ba6f-bd97ae7cae85, 40079818-3808-4585-903b-02605f061225" disabled>
   </mgt-people-picker>
-  </mgt-people-picker>
 `;
 
 export const pickerDefaultSelectedUserIds = () => html`
