956e7683a8cf8c276eb96d8bf0e6881bec5c6faa
beth-panx <beth_panx@hotmail.com>
2021-02-23 17:06:55 -0800

    Add license to file graph file

--- packages/mgt-components/src/graph/graph.files.ts
+++ packages/mgt-components/src/graph/graph.files.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 import { IGraph, prepScopes } from '@microsoft/mgt-element';
 import { DriveItem } from '@microsoft/microsoft-graph-types';
 import { UsedInsight } from '@microsoft/microsoft-graph-types-beta';
