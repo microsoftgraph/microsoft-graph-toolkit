4897b78c8170b154aead33af17b927f27aad830e
beth-panx <beth_panx@hotmail.com>
2021-02-23 17:12:47 -0800

    Remove unused graph function

--- packages/mgt-components/src/graph/graph.files.ts
+++ packages/mgt-components/src/graph/graph.files.ts
@@ -7,25 +7,6 @@
 
 import { IGraph, prepScopes } from '@microsoft/mgt-element';
 import { DriveItem } from '@microsoft/microsoft-graph-types';
-import { UsedInsight } from '@microsoft/microsoft-graph-types-beta';
-
-/**
- * Represents an insight item from the Graph Insights API
- */
-export type InsightsItem = UsedInsight;
-
-/**
- * Get the user's insight items
- *
- * @export
- * @param {IGraph} graph
- * @param {InsightsDataSource} dataSource
- * @returns {Promise<InsightItem>}
- */
-export async function getMyInsights(graph: IGraph): Promise<InsightsItem[]> {
-  let response = await graph.api('/me/insights/used').get();
-  return response.value || null;
-}
 
 export async function getDriveItemByQuery(graph: IGraph, resource: string): Promise<DriveItem> {
   const scopes = 'Files.Read';
