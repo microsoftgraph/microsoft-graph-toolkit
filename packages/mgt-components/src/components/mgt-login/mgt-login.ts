56d2c62d85e0d62692eec63a6c1fbd5d4d60ba89
Nikola Metulev <nmetulev@users.noreply.github.com>
2021-04-27 21:13:34 -0700

    Update packages/mgt-components/src/components/mgt-login/mgt-login.ts

--- packages/mgt-components/src/components/mgt-login/mgt-login.ts
+++ packages/mgt-components/src/components/mgt-login/mgt-login.ts
@@ -421,7 +421,7 @@ export class MgtLogin extends MgtTemplatedComponent {
    * @param {*} account
    * @memberof MgtLogin
    */
-  setActiveAccount(account: IProviderAccount) {
+  private setActiveAccount(account: IProviderAccount) {
     Providers.globalProvider.setActiveAccount(account);
   }
 
