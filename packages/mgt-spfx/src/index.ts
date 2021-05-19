20ff3a33afe50afa901e2993a48202e19b7706a5
Nikola Metulev <nikolame@microsoft.com>
2021-05-18 23:02:43 -0700

    added mgt-react as a dependency of mgt-spfx

--- packages/mgt-spfx/src/index.ts
+++ packages/mgt-spfx/src/index.ts
@@ -2,3 +2,4 @@ export { MgtLibrary } from './libraries/mgt/MgtLibrary';
 export * from '@microsoft/mgt-sharepoint-provider';
 export * from '@microsoft/mgt-components';
 export * from '@microsoft/mgt-element';
+export * from '@microsoft/mgt-react';
