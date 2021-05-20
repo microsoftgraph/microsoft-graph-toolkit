5e0c0f1276ac205a51b4caa1cc6301e58127ba54
Nikola Metulev <nikolame@microsoft.com>
2021-05-19 17:10:52 -0700

    sp-webpart sample cleanup

--- samples/sp-webpart/src/webparts/mgtDemo/MgtDemoWebPart.ts
+++ samples/sp-webpart/src/webparts/mgtDemo/MgtDemoWebPart.ts
@@ -4,16 +4,6 @@ import { Version } from '@microsoft/sp-core-library';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane';
 
-// import web component polyfills for browsers that need them
-import 'regenerator-runtime/runtime';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import '@webcomponents/webcomponentsjs/webcomponents-bundle.js';
-
 import * as strings from 'MgtDemoWebPartStrings';
 import MgtDemo from './components/MgtDemo';
 import { IMgtDemoProps } from './components/IMgtDemoProps';
@@ -26,19 +16,15 @@ export interface IMgtDemoWebPartProps {
 }
 
 export default class MgtDemoWebPart extends BaseClientSideWebPart<IMgtDemoWebPartProps> {
-
   // set the global provider
   protected async onInit() {
     Providers.globalProvider = new SharePointProvider(this.context);
   }
 
   public render(): void {
-    const element: React.ReactElement<IMgtDemoProps> = React.createElement(
-      MgtDemo,
-      {
-        description: this.properties.description
-      }
-    );
+    const element: React.ReactElement<IMgtDemoProps> = React.createElement(MgtDemo, {
+      description: this.properties.description
+    });
 
     ReactDom.render(element, this.domElement);
   }
