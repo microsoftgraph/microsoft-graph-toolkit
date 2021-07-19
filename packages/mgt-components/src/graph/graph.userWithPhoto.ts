5e1e8e2264630c3613dc80bc4eb7a38263ffb1d2
NathZ1 <nath_zipf@hotmail.com>
2021-07-20 07:34:51 +1000

    Update timeCache of cached users without photo following recheck for photo via graph (#1247)
    * Update timeCache of cached users without photo following recheck for photo via graph
    
    * Update packages/mgt-components/src/graph/graph.userWithPhoto.ts
    
    Co-authored-by: Nikola Metulev <nmetulev@users.noreply.github.com>
    Co-authored-by: Beth Pan <beth_panx@hotmail.com>

--- packages/mgt-components/src/graph/graph.userWithPhoto.ts
+++ packages/mgt-components/src/graph/graph.userWithPhoto.ts
@@ -73,7 +73,12 @@ export async function getUserWithPhoto(
         } else {
           cachedPhoto = null;
         }
-      } catch (e) {}
+      } catch (e) {
+        //if 404 received (photo not found) but user already in cache, update timeCache value to prevent repeated 404 error / graph calls on each page refresh
+        if (e.code === 'ErrorItemNotFound') {
+          storePhotoInCache(userId || 'me', schemas.photos.stores.users, { eTag: null, photo: null });
+        }
+      }
     }
   }
 
