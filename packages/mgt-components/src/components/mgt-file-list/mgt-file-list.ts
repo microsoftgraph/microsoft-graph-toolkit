d6bf62ad7b2beaaff6323d46adf687c4c0518cf4
Nikola Metulev <nikolame@microsoft.com>
2021-08-30 21:34:11 -0700

    downgraded to @fluentui/web-componenxts@0.22

--- packages/mgt-components/src/components/mgt-file-list/mgt-file-list.ts
+++ packages/mgt-components/src/components/mgt-file-list/mgt-file-list.ts
@@ -41,10 +41,12 @@ import { strings } from './strings';
 import { MgtFile } from '../mgt-file/mgt-file';
 import { MgtFileUploadConfig } from './mgt-file-upload/mgt-file-upload';
 
-import { fluentProgressRing } from '@fluentui/web-components';
-import { registerFluentComponents } from '../../utils/FluentComponents';
+export { FluentDesignSystemProvider, FluentProgressRing } from '@fluentui/web-components';
 
-registerFluentComponents(fluentProgressRing);
+// import { fluentProgressRing } from '@fluentui/web-components';
+// import { registerFluentComponents } from '../../utils/FluentComponents';
+
+// registerFluentComponents(fluentProgressRing);
 
 /**
  * The File List component displays a list of multiple folders and files by
@@ -573,6 +575,7 @@ export class MgtFileList extends MgtTemplatedComponent {
    */
   protected renderFiles(): TemplateResult {
     return html`
+    <fluent-design-system-provider use-defaults>
       <div id="file-list-wrapper" class="file-list-wrapper" dir=${this.direction}>
         ${this.enableFileUpload ? this.renderFileUpload() : null}
         <ul
@@ -599,6 +602,7 @@ export class MgtFileList extends MgtTemplatedComponent {
             : null
         }
       </div>
+    </fluent-design-system-provider>
     `;
   }
 
