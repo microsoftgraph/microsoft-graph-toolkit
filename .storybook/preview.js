2e77f277a482d52ba1c96318740478a5742de8da
Japheth Obala <japhethobalak@gmail.com>
2021-11-18 11:02:56 +0300

    uncomments HMR

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -26,12 +26,12 @@ addons.setConfig({
 });
 
 // force full reload to not reregister web components
-// const req = require.context('../stories', true, /\.(js|mdx)$/);
-// configure(req, module);
-// if (module.hot) {
-//   module.hot.accept(req.id, () => {
-//     const currentLocationHref = window.location.href;
-//     window.history.pushState(null, null, currentLocationHref);
-//     window.location.reload();
-//   });
-// }
+const req = require.context('../stories', true, /\.(js|mdx)$/);
+configure(req, module);
+if (module.hot) {
+  module.hot.accept(req.id, () => {
+    const currentLocationHref = window.location.href;
+    window.history.pushState(null, null, currentLocationHref);
+    window.location.reload();
+  });
+}
