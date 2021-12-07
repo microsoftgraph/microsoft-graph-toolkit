b5c3aece901bf1d1386799972eb8e123c19edcde
jobala <japhethobalak@gmail.com>
2021-12-07 11:54:10 +0300

    uses correct key for spacebar

--- packages/mgt-components/src/components/mgt-tasks-base/mgt-tasks-base.ts
+++ packages/mgt-components/src/components/mgt-tasks-base/mgt-tasks-base.ts
@@ -250,7 +250,7 @@ export abstract class MgtTasksBase extends MgtTemplatedComponent {
             class="TaskIcon TaskCancel"
             @click="${() => this.hideNewTaskPanel()}"
             @keypress="${(e: KeyboardEvent) => {
-              if (e.key === 'Enter' || e.key === 'Space') this.hideNewTaskPanel();
+              if (e.key === 'Enter' || e.key === ' ') this.hideNewTaskPanel();
             }}">
             <span>${this.strings.cancelNewTaskSubtitle}</span>
           </div>
@@ -259,7 +259,7 @@ export abstract class MgtTasksBase extends MgtTemplatedComponent {
             class="TaskIcon TaskAdd"
             @click="${() => this.addTask()}"
             @keypress="${(e: KeyboardEvent) => {
-              if (e.key === 'Enter' || e.key === 'Space') this.addTask();
+              if (e.key === 'Enter' || e.key === ' ') this.addTask();
             }}"
           >
             <span></span>
