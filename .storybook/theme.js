d5bea5f01ee3dbf544fd393f7308dae523f5b274
jobala <japhethobalak@gmail.com>
2021-12-08 11:10:00 +0300

    updates storybook theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,33 +3,33 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
 
-  //   colorPrimary: 'hotpink',
-  //   colorSecondary: 'deepskyblue',
+  // colorPrimary: 'hotpink',
+  colorSecondary: '#0360B9',
 
   // UI
-  //   appBg: 'white',
-  //   appContentBg: 'silver',
-  //   appBorderColor: 'grey',
-  //   appBorderRadius: 4,
+  // appBg: 'white',
+  // appContentBg: 'silver',
+  // appBorderColor: 'grey',
+  // appBorderRadius: 4,
 
   // Typography
-  //   fontBase: '"Open Sans", sans-serif',
-  //   fontCode: 'monospace',
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: 'monospace',
 
   // Text colors
-  //   textColor: 'black',
-  //   textInverseColor: 'rgba(255,255,255,0.9)',
+  textColor: '#1A1A1A',
+  textInverseColor: 'rgba(255,255,255,0.9)',
 
   // Toolbar default and active colors
-  //   barTextColor: 'silver',
-  //   barSelectedColor: 'black',
-  //   barBg: 'hotpink',
+  barTextColor: '#717171',
+  // barSelectedColor: '#0360B9',
+  // barBg: 'hotpink',
 
   // Form colors
-  //   inputBg: 'white',
-  //   inputBorder: 'silver',
-  //   inputTextColor: 'black',
-  //   inputBorderRadius: 4,
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
 
   brandTitle: 'Microsoft Graph Toolkit Playground',
   brandUrl: 'https://aka.ms/mgt'
