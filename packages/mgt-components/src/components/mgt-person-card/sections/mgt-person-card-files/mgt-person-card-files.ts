4fc87439a6b6bc405f6e3c4098da12b2d013917e
Musale Martin <martinmusale@microsoft.com>
2022-04-06 13:51:12 +0300

    Add alt-text for file-icon image

--- packages/mgt-components/src/components/mgt-person-card/sections/mgt-person-card-files/mgt-person-card-files.ts
+++ packages/mgt-components/src/components/mgt-person-card/sections/mgt-person-card-files/mgt-person-card-files.ts
@@ -148,7 +148,7 @@ export class MgtPersonCardFiles extends BasePersonCardSection {
     return html`
       <div class="file" @click=${e => this.handleFileClick(file)}>
         <div class="file__icon">
-          <img src=${getFileTypeIconUri(file.resourceVisualization.type, 48, 'svg')} />
+          <img alt="${file.resourceVisualization.title}" src=${getFileTypeIconUri(file.resourceVisualization.type, 48, 'svg')} />
         </div>
         <div class="file__details">
           <div class="file__name">${file.resourceVisualization.title}</div>
