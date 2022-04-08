079c3355b6336f0f41e575d324badaa5838ace56
Mnickii <miaronkirote@gmail.com>
2022-04-08 15:52:51 +0300

    update person events and templating stories

--- stories/components/person/person.js
+++ stories/components/person/person.js
@@ -21,34 +21,42 @@ export const person = () => html`
 export const events = () => html`
   <div style="margin-bottom: 10px">Click on each line</div>
   <div class="example">
-    <mgt-person person-query="me" view="threelines"></mgt-person>
+    <mgt-person person-query="me" view="fourlines"></mgt-person>
   </div>
 
   <div class="output">no line clicked</div>
 
   <script>
-    const person = document.querySelector('mgt-person');
-    person.addEventListener('line1clicked', e => {
+  const person = document.querySelector('mgt-person');
+  person.addEventListener('line1clicked', e => {
       const output = document.querySelector('.output');
 
       if (e && e.detail && e.detail.displayName) {
-        output.innerHTML = '<b>line1clicked:</b> ' + e.detail.displayName;
+          output.innerHTML = '<b>line1clicked:</b> ' + e.detail.displayName;
       }
-    });
-    person.addEventListener('line2clicked', e => {
-      const output = document.querySelector('.output');
-
-      if (e && e.detail && e.detail.mail) {
-        output.innerHTML = '<b>line2clicked:</b> ' + e.detail.mail;
-      }
-    });
-    person.addEventListener('line3clicked', e => {
+  });
+  person.addEventListener('line2clicked', e => {
       const output = document.querySelector('.output');
 
       if (e && e.detail && e.detail.jobTitle) {
-        output.innerHTML = '<b>line3clicked:</b> ' + e.detail.jobTitle;
+          output.innerHTML = '<b>line2clicked:</b> ' + e.detail.jobTitle;
       }
-    });
+  });
+  person.addEventListener('line3clicked', e => {
+      const output = document.querySelector('.output');
+
+      if (e && e.detail && e.detail.department) {
+          output.innerHTML = '<b>line3clicked:</b> ' + e.detail.department;
+      }
+  });
+  person.addEventListener('line4clicked', e => {
+      const output = document.querySelector('.output');
+
+      if (e && e.detail && e.detail.mail) {
+          output.innerHTML = '<b>line4clicked:</b> ' + e.detail.mail;
+      }
+  });
+
   </script>
 
   <style>
