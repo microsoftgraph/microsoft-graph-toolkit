2f226de4e4a3811a68cf04dfa6b882ecf79cb9e6
Mnickii <miaronkirote@gmail.com>
2022-05-09 17:42:12 +0300

    update person-card styling

--- packages/mgt-components/src/components/mgt-person-card/mgt-person-card.ts
+++ packages/mgt-components/src/components/mgt-person-card/mgt-person-card.ts
@@ -106,6 +106,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     }
 
     if (this.config.sections.mailMessages) {
+      scopes.push('Mail.Read');
       scopes.push('Mail.ReadBasic');
     }
 
@@ -1182,6 +1183,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     }
 
     const contactSections = new MgtPersonCardContact(this.internalPersonDetails as User);
+    console.log('contactSessions: ', contactSections);
     if (contactSections.hasData) {
       this.sections.push(contactSections);
     }
@@ -1189,6 +1191,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
     if (!this.state) {
       return;
     }
+    console.log('state', this.state);
 
     const { person, directReports, messages, files, profile } = this.state;
 
@@ -1213,6 +1216,7 @@ export class MgtPersonCard extends MgtTemplatedComponent {
         this.sections.push(profileSection);
       }
     }
+    console.log(this.sections);
   }
 
   private getImage(): string {
