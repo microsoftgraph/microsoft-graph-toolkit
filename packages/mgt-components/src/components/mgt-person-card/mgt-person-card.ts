be02747a71a6a84f9b626dfabaeb9037988868cb
Mnickii <miaronkirote@gmail.com>
2022-05-10 12:47:44 +0300

    update quick message styling

--- packages/mgt-components/src/components/mgt-person-card/mgt-person-card.ts
+++ packages/mgt-components/src/components/mgt-person-card/mgt-person-card.ts
@@ -825,27 +825,27 @@ export class MgtPersonCard extends MgtTemplatedComponent {
       );
     }
 
-    // To be included when support for direct messaging is added
-    // ${this.internalPersonDetails.id !== this._me.id && MgtPersonCard.config.isSendMessageVisible
-    //   ? html`
-    //       <div class="quick-message">
-    //         <input
-    //           type="text"
-    //           class="quick-message__input"
-    //           placeholder="Message ${this.internalPersonDetails.displayName}"
-    //           .value=${this._chatInput}
-    //           @input=${(e: Event) => {
-    //             this._chatInput = (e.target as HTMLInputElement).value;
-    //           }}
-    //         />
-    //         <button class="quick-message__send" @click=${() => this.sendQuickMessage()}>
-    //           ${getSvg(SvgIcon.Send)}
-    //         </button>
-    //       </div>
-    //     `
-    //   : null}
-
     return html`
+    ${
+      this.internalPersonDetails.id !== this._me.id && MgtPersonCard.config.isSendMessageVisible
+        ? html`
+          <div class="quick-message">
+            <input
+              type="text"
+              class="quick-message__input"
+              placeholder=${this.strings.quickMessagePlaceholder}
+              .value=${this._chatInput}
+              @input=${(e: Event) => {
+                this._chatInput = (e.target as HTMLInputElement).value;
+              }}
+            />
+            <button class="quick-message__send" @click=${() => this.sendQuickMessage()}>
+              ${getSvg(SvgIcon.Send)}
+            </button>
+          </div>
+        `
+        : null
+    }
       <div class="sections">
         <div class="message-section">
           ${this.renderMessagingSection()}
