7c3b96cbd427a9d8abfb71fe5f7e6a190a897599
Musale Martin <martinmusale@microsoft.com>
2022-05-11 11:37:18 +0300

    Fix searching users from a list of user-ids

--- packages/mgt-components/src/components/mgt-people-picker/mgt-people-picker.ts
+++ packages/mgt-components/src/components/mgt-people-picker/mgt-people-picker.ts
@@ -1040,7 +1040,12 @@ export class MgtPeoplePicker extends MgtTemplatedComponent {
                   people = await findUsers(graph, input, this.showMax, this._userFilters);
                 }
               } else {
-                people = (await findPeople(graph, input, this.showMax, this.userType, this._peopleFilters)) || [];
+                if (this.userIds && this.userIds.length) {
+                  // has the user-ids proerty set
+                  people = await getUsersForUserIds(graph, this.userIds, input, this._userFilters);
+                } else {
+                  people = (await findPeople(graph, input, this.showMax, this.userType, this._peopleFilters)) || [];
+                }
               }
             } catch (e) {
               // nop
@@ -1676,6 +1681,7 @@ export class MgtPeoplePicker extends MgtTemplatedComponent {
     // ensuring people list is displayed
     // find ids from selected people
     if (people && people.length > 0) {
+      people = people.filter(person => person);
       const idFilter = this.selectedPeople.map(el => {
         return el.id ? el.id : el.displayName;
       });
