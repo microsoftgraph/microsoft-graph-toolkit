fba26b6741e39a6ec6447ac89635092543a6e128
Musale Martin <martinmusale@microsoft.com>
2022-05-16 14:48:13 +0300

    Add displayName value for fallback-details story

--- stories/components/person/person.properties.js
+++ stories/components/person/person.properties.js
@@ -52,7 +52,7 @@ export const personFallbackDetails = () => html`
     <mgt-person
       person-query="mbowen"
       view="twoLines"
-      fallback-details='{"mail":"MeganB@M365x214355.onmicrosoft.com", "displayName":" Megan Bowen"}'
+      fallback-details='{"mail":"MeganB@M365x214355.onmicrosoft.com", "displayName":"Megan Bowen"}'
     ></mgt-person>
   </div>
   <div class="example">
