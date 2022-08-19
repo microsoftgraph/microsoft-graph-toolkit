5fc1281b94178e7e4fa5436d69da6bd85d5eef65
Gavin Barron <gavinbarron@microsoft.com>
2022-08-19 12:43:27 -0700

    adding screenshots to three line test

--- packages/mgt-react/playwright-ct.config.ts
+++ packages/mgt-react/playwright-ct.config.ts
@@ -32,9 +32,15 @@ const config: PlaywrightTestConfig = {
   projects: [
     {
       name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome']
-      }
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
     }
   ]
 };
--- packages/mgt-react/specs/Person.spec.tsx
+++ packages/mgt-react/specs/Person.spec.tsx
@@ -13,46 +13,52 @@ import React from 'react';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
+test('should show name, image, email and title for three line view', async ({ mount, page }) => {
+  const component = await mount(<Person personQuery="me" view={5}></Person>);
+
+  await expect(component.locator('.line1')).toHaveText('Megan Bowen');
+  await expect(component.locator('img')).toHaveAttribute('alt', 'Megan Bowen');
+  await expect(component.locator('.line2')).toHaveText('MeganB@M365x214355.onmicrosoft.com');
+  await expect(component.locator('.line3')).toHaveText('Auditor');
+  await expect(page).toHaveScreenshot();
+});
+
 test('should show name, image and title for two line view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={4}></Person>);
 
-  await expect(component.locator('div.line1')).toHaveText('Megan Bowen');
-  await expect(component.locator('div.line2')).toHaveText('MeganB@M365x214355.onmicrosoft.com');
-  expect(component.locator('img[alt="Megan Bowen"]')).toBeTruthy();
+  await expect(component.locator('img')).toHaveAttribute('alt', 'Megan Bowen');
+  await expect(component.locator('.line1')).toHaveText('Megan Bowen');
+  await expect(component.locator('.line2')).toHaveText('MeganB@M365x214355.onmicrosoft.com');
 });
 
 test('should not show line 3 for two line view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={3}></Person>);
 
   test.fail();
-  await expect(component.locator('div.line3')).toBeDefined();
+  await expect(component.locator('.line3')).toHaveText('Auditor');
 });
 
 test('should show name and image for one line view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={3}></Person>);
 
-  await expect(component.locator('div.line1')).toHaveText('Megan Bowen');
-  expect(component.locator('img[alt="Megan Bowen"]')).toBeTruthy();
+  await expect(component.locator('img')).toHaveAttribute('alt', 'Megan Bowen');
+  await expect(component.locator('.line1')).toHaveText('Megan Bowen');
 });
 
 test('should not show line 2 for one line view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={3}></Person>);
-  await page.pause();
   test.fail();
-  const line2 = component.locator('div.line2');
-  console.log(JSON.stringify(line2));
-  await page.pause();
-  await expect(line2).toHaveText('MeganB@M365x214355.onmicrosoft.com');
+  await expect(component.locator('div.line2')).toHaveText('MeganB@M365x214355.onmicrosoft.com');
 });
 
 test('should show image for image view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={2}></Person>);
 
-  expect(component.locator('img[alt="Megan Bowen"]')).toBeTruthy();
+  await expect(component.locator('img')).toHaveAttribute('alt', 'Megan Bowen');
 });
 
 test('should not name for image view', async ({ mount, page }) => {
   const component = await mount(<Person personQuery="me" view={2}></Person>);
   test.fail();
-  await expect(component.locator('div.line1')).toHaveText('Megan Bowen');
+  await expect(component.locator('.line1')).toHaveText('Megan Bowen');
 });
