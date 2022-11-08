9a7d2d1cf8aa71717670429b378420b03ed47e74
Musale Martin <martinmusale@microsoft.com>
2022-11-08 14:58:09 +0300

    feat: change to validating the baseUrl before initializing the client

--- packages/mgt-element/src/Graph.ts
+++ packages/mgt-element/src/Graph.ts
@@ -21,7 +21,7 @@ import { IGraph, MICROSOFT_GRAPH_DEFAULT_ENDPOINT } from './IGraph';
 import { IProvider } from './providers/IProvider';
 import { Batch } from './utils/Batch';
 import { ComponentMiddlewareOptions } from './utils/ComponentMiddlewareOptions';
-import { chainMiddleware } from './utils/GraphHelpers';
+import { chainMiddleware, validateBaseURL } from './utils/GraphHelpers';
 import { SdkVersionMiddleware } from './utils/SdkVersionMiddleware';
 import { PACKAGE_VERSION } from './utils/version';
 
@@ -155,13 +155,15 @@ export function createFromProvider(provider: IProvider, version?: string, compon
     new HTTPMessageHandler()
   ];
 
-  const baseURL = provider.baseURL ? provider.baseURL : MICROSOFT_GRAPH_DEFAULT_ENDPOINT;
+  let baseURL = provider.baseURL ? provider.baseURL : MICROSOFT_GRAPH_DEFAULT_ENDPOINT;
+  const validURL = validateBaseURL(baseURL);
+  if (validURL) {
+    const client = Client.initWithMiddleware({
+      middleware: chainMiddleware(...middleware),
+      baseUrl: validURL
+    });
 
-  const client = Client.initWithMiddleware({
-    middleware: chainMiddleware(...middleware),
-    baseUrl: baseURL
-  });
-
-  const graph = new Graph(client, version);
-  return component ? graph.forComponent(component) : graph;
+    const graph = new Graph(client, version);
+    return component ? graph.forComponent(component) : graph;
+  }
 }
