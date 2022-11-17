f6183334854a1bfaa65973d1749663e0301ed982
Nickii Miaro <miaronkirote@gmail.com>
2022-11-17 20:17:50 +0300

    Update packages/mgt-components/src/components/mgt-person-card/mgt-person-card.graph.ts
    Co-authored-by: Musale Martin <martinmusale@microsoft.com>

--- packages/mgt-components/src/components/mgt-person-card/mgt-person-card.graph.ts
+++ packages/mgt-components/src/components/mgt-person-card/mgt-person-card.graph.ts
@@ -195,7 +195,7 @@ export async function createChat(
  * @param {{ body: {"content": string}  }} messageData
  * @return {*}  {Promise<ChatMessage>}
  */
-export async function sendMesage(
+export async function sendMessage(
   graph: IGraph,
   // tslint:disable-next-line: completed-docs
   chatId: string,
