3680b9df7dd02760a9cf9ec73e704bdcbbb36948
Gavin Barron <gavinbarron@microsoft.com>
2023-01-23 15:33:17 -0800

    docs: adds version information to storybook

--- stories/samples/editor.stories.js
+++ stories/samples/editor.stories.js
@@ -7,15 +7,19 @@
 
 import { html } from 'lit-element';
 import { withCodeEditor } from '../../.storybook/addons/codeEditorAddon/codeAddon';
+import { versionInfo } from '../versionInfo';
 
 export default {
+  parameters: {
+    version: versionInfo
+  },
   title: 'Editor',
   decorators: [withCodeEditor]
 };
 
 export const Editor = () => html`
   <!-- Add your own HTML code here -->
-  
+
   <script>
     // Add your own JavaScript code here
   </script>
---
+++ stories/versionInfo.js
@@ -0,0 +1,16 @@
+import { PACKAGE_VERSION } from '@microsoft/mgt-element';
+
+const postfixSplit = PACKAGE_VERSION.indexOf('-');
+let version = PACKAGE_VERSION;
+let postfix;
+if (postfixSplit > 0) {
+  version = PACKAGE_VERSION.substring(0, postfixSplit);
+  postfix = PACKAGE_VERSION.substring(postfixSplit + 1);
+}
+const [major, minor, patch] = version.split('.');
+export const versionInfo = {
+  major,
+  minor,
+  patch,
+  postfix
+};
