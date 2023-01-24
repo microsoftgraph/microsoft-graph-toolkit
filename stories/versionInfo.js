5615d9e0a11ea5690511fae61b57d2764c540101
Gavin Barron <gavinbarron@microsoft.com>
2023-01-23 16:00:14 -0800

    debugging pipeline changes

--- stories/versionInfo.js
+++ stories/versionInfo.js
@@ -1,3 +1,9 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
 import { PACKAGE_VERSION } from '@microsoft/mgt-element';
 
 const postfixSplit = PACKAGE_VERSION.indexOf('-');
