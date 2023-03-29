44a4a944dca62b95ded303cb42a957e818916918
Gavin Barron <gavinbarron@microsoft.com>
2023-03-29 21:57:43 +0000

    updating client_id to use torus app

--- .storybook/env.js
+++ .storybook/env.js
@@ -1,4 +1,4 @@
-export const CLIENTID = '6b5a42f1-9e1a-4a20-9df2-9b058df3c213';
+export const CLIENTID = '9aeac104-a6b6-45ca-bb7c-bf7c3083eeb2';
 export const GETPROVIDER_EVENT = 'mgt/getProvider';
 export const SETPROVIDER_EVENT = 'mgt/setProvider';
 export const AUTH_PAGE = 'blank.html';
