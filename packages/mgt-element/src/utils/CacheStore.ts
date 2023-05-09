bd322ba6f3bbe81b0627bd5161ac015a6f17452d
Gavin Barron <gavinbarron@microsoft.com>
2023-05-09 18:53:13 +0000

    adding copyright headers

--- packages/mgt-element/src/utils/CacheStore.ts
+++ packages/mgt-element/src/utils/CacheStore.ts
@@ -1,3 +1,10 @@
+/**
+ * -------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
+ * See License in the project root for license information.
+ * -------------------------------------------------------------------------------------------
+ */
+
 import { openDB } from 'idb';
 import { Providers } from '../providers/Providers';
 import { CacheItem, CacheSchema, dbListKey } from './CacheService';
