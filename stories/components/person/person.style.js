71aef6bca5bc7d605be9d540c27556c64d520707
Nickii Miaro <miaronkirote@gmail.com>
2023-05-29 11:47:15 +0300

    fix: person text visibility in custom properties (#2298)
    * fix line4 text visibility in custom properties
    
    * fix line-height to prevent overlap
    
    * Apply suggestions from code review
    
    Co-authored-by: Musale Martin <martinmusale@microsoft.com>
    
    ---------
    
    Co-authored-by: Musale Martin <martinmusale@microsoft.com>

--- stories/components/person/person.style.js
+++ stories/components/person/person.style.js
@@ -76,31 +76,32 @@ export const customCSSProperties = () => html`
       --person-initials-text-color: white;
       --person-initials-background-color: blue;
 
-      --person-line1-font-size: 32px;
+      --person-line1-font-size: 20px;
       --person-line1-font-weight: 600;
       --person-line1-text-color: red;
       --person-line1-text-transform: capitalize;
-      --person-line1-text-line-height: 20px;
+      --person-line1-text-line-height: 22px;
 
-      --person-line2-font-size: 28px;
+      --person-line2-font-size: 18px;
       --person-line2-font-weight: 500;
       --person-line2-text-color: orange;
       --person-line2-text-transform: full-width;
-      --person-line2-text-line-height: 16px
+      --person-line2-text-line-height: 20px;
 
-      --person-line3-font-size: 24px;
+      --person-line3-font-size: 16px;
       --person-line3-font-weight: 400;
       --person-line3-text-color: blue;
       --person-line3-text-transform: uppercase;
-      --person-line3-text-line-height: 12px;
+      --person-line3-text-line-height: 18px;
 
-      --person-line4-font-size: 20px;
+      --person-line4-font-size: 14px;
       --person-line4-font-weight: 300;
-      --person-line4-text-color: green;
+      --person-line4-text-color: black;
       --person-line4-text-transform: lowercase;
-      --person-line4-text-line-height: 12px;
+      --person-line4-text-line-height: 16px;
 
       --person-details-spacing: 30px;
+      --person-details-bottom-spacing: 20px;
     }
   </style>
 
