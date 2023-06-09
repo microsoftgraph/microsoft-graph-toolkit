3646ede860ccbff1271f9761bfb11d9f8be37893
Gavin Barron <gavinbarron@microsoft.com>
2023-06-09 21:51:56 +0000

    fix: max picker list height
    adds a new variable to set the max height of the mgt-picker list
    defaults the max height of the mgt-picker list to 380px

--- packages/mgt-components/src/components/mgt-picker/mgt-picker.ts
+++ packages/mgt-components/src/components/mgt-picker/mgt-picker.ts
@@ -15,6 +15,7 @@ import { registerFluentComponents } from '../../utils/FluentComponents';
 import '../../styles/style-helper';
 import { Entity } from '@microsoft/microsoft-graph-types';
 import { DataChangedDetail } from '../mgt-get/mgt-get';
+import { styles } from './mgt-picker-css';
 
 registerFluentComponents(fluentCombobox, fluentOption);
 
@@ -27,11 +28,16 @@ registerFluentComponents(fluentCombobox, fluentOption);
  * @extends {MgtTemplatedComponent}
  *
  * @cssprop --picker-background-color - {Color} Picker component background color
+ * @cssprop --picker-list-max-height - {String} max height for options list
  */
 @customElement('picker')
 export class MgtPicker extends MgtTemplatedComponent {
   protected get strings() {
     return strings;
+  }
+
+  public static get styles() {
+    return styles;
   }
 
   /**
