1838eef12dded95e6e5eb5591fe42a85917f1116
Martin Musale <martinmusale@microsoft.com>
2023-07-25 11:46:35 +0300

    Add the optional title to text template with no image
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- packages/mgt-components/src/components/mgt-person/mgt-person.ts
+++ packages/mgt-components/src/components/mgt-person/mgt-person.ts
@@ -726,6 +726,7 @@ export class MgtPerson extends MgtTemplatedComponent {
     // this reduces the redundant announcement of the user's name.
     const textTemplate = html`
       <span 
+        title="${ifDefined(imageOnly ? titleText : undefined)}"
         role="${ifDefined(this.view === ViewType.image ? undefined : 'presentation')}"
         class="${textClasses}"
       >
