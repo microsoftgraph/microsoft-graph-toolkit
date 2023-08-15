ff7ec7ceaaf4d5ecb3ed1716b7a495eabca78677
Musale Martin <martinmusale@microsoft.com>
2023-08-15 13:19:43 +0300

    Add styling for the unsupported content component
    Signed-off-by: Musale Martin <martinmusale@microsoft.com>

--- packages/mgt-chat/src/components/UnsupportedContent/UnsupportedContent.tsx
+++ packages/mgt-chat/src/components/UnsupportedContent/UnsupportedContent.tsx
@@ -4,14 +4,44 @@
  * See License in the project root for license information.
  * -------------------------------------------------------------------------------------------
  */
+import { makeStyles, shorthands } from '@fluentui/react-components';
 import React from 'react';
+import { registerAppIcons } from '../styles/registerIcons';
+import { ArrowSquareUpRight24Regular } from '@fluentui/react-icons';
+registerAppIcons();
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: '#ebebeb',
+    display: 'flex',
+    boxShadow: '0px 4px 8px 0px rgba(0, 0, 0, 0.14), 0px 0px 2px 0px rgba(0, 0, 0, 0.12)',
+    textDecoration: 'none',
+    color: '#424242',
+    ...shorthands.margin('4px 0 0 0'),
+    ...shorthands.borderRadius('6px'),
+    ...shorthands.padding('16px'),
+    ...shorthands.gap('6px'),
+    ':hover': {
+      backgroundColor: '#fafafa'
+    }
+  },
+  cta: {
+    fontFamily: 'Segoe UI',
+    fontSize: '12px',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: '16px'
+  }
+});
 
 const UnsupportedContent = () => {
+  const styles = useStyles();
   return (
-    <div>
-      <a href="https://teams.microsoft.com">X</a>
-      <p>Go back to the main window to see this content.</p>
-    </div>
+    // TODO: update this URL to the correct value.
+    <a className={styles.container} href="https://teams.microsoft.com">
+      <ArrowSquareUpRight24Regular />
+      <p className={styles.cta}>Go back to the main window to see this content.</p>
+    </a>
   );
 };
 
