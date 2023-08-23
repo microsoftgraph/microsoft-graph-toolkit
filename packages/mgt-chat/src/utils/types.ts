ba18bcc95c3e138e6a76c36ec828e8a9260fd966
Martin Musale <martinmusale@microsoft.com>
2023-08-23 15:20:21 +0300

    Add a types utility
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

---
+++ packages/mgt-chat/src/utils/types.ts
@@ -0,0 +1,20 @@
+import { ChatMessage, Message } from '@azure/communication-react';
+import { GraphChatMessage } from 'src/statefulClient/StatefulGraphChatClient';
+
+/**
+ * A typeguard to get the ChatMessage type
+ * @param msg of Message
+ * @returns ChatMessage
+ */
+export const isChatMessage = (msg: Message): msg is ChatMessage => {
+  return 'content' in msg;
+};
+
+/**
+ * A typeguard to get the GraphChatMessage type
+ * @param msg of Message
+ * @returns GraphChatMessage
+ */
+export const isGraphChatMessage = (msg: Message): msg is GraphChatMessage => {
+  return 'content' in msg && 'hasUnsupportedContent' in msg && 'rawChatUrl' in msg;
+};
