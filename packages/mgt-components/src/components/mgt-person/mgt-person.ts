5c50d22eb1b902b3ac01541027eb073643529d51
Gavin Barron <gavinbarron@microsoft.com>
2023-10-05 21:04:55 +0000

    fixing render problem when given and surname are null on personType objects

--- packages/mgt-components/src/components/mgt-person/mgt-person.ts
+++ packages/mgt-components/src/components/mgt-person/mgt-person.ts
@@ -261,10 +261,7 @@ export class MgtPerson extends MgtTemplatedComponent {
    *
    * @type {IDynamicPerson}
    */
-  @property({
-    attribute: null,
-    type: Object
-  })
+  @state()
   private get personDetailsInternal(): IDynamicPerson {
     return this._personDetailsInternal;
   }
@@ -1216,8 +1213,8 @@ export class MgtPerson extends MgtTemplatedComponent {
 
     let initials = '';
     if (isUser(person)) {
-      initials += person.givenName[0].toUpperCase();
-      initials += person.surname[0].toUpperCase();
+      initials += person.givenName?.[0].toUpperCase() ?? '';
+      initials += person.surname?.[0].toUpperCase() ?? '';
     }
 
     if (!initials && person.displayName) {
