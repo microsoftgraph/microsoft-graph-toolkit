b153db33ae4c8467e9dddd93675c9d02ae5261e9
Gavin Barron <gavinbarron@microsoft.com>
2023-10-10 16:42:17 +0000

    update list of components registered by registerMgtComponents

--- packages/mgt-components/src/registerMgtComponents.ts
+++ packages/mgt-components/src/registerMgtComponents.ts
@@ -1,25 +1,43 @@
 import {
   registerMgtAgendaComponent,
-  registerMgtContactComponent,
   registerMgtFileComponent,
+  registerMgtFileListComponent,
+  registerMgtGetComponent,
   registerMgtLoginComponent,
   registerMgtPeopleComponent,
+  registerMgtPeoplePickerComponent,
   registerMgtPersonCardComponent,
   registerMgtPersonComponent,
-  registerMgtSpinnerComponent
+  registerMgtPickerComponent,
+  registerMgtSearchBoxComponent,
+  registerMgtSearchResultsComponent,
+  registerMgtSpinnerComponent,
+  registerMgtTasksComponent,
+  registerMgtTaxonomyPickerComponent,
+  registerMgtTeamsChannelPickerComponent,
+  registerMgtThemeToggleComponent,
+  registerMgtTodoComponent
 } from './components/components';
-import { registerMgtFlyoutComponent } from './components/sub-components/mgt-flyout/mgt-flyout';
 
 export const registerMgtComponents = () => {
   // this should match the set of components listed for export in packages/mgt-react/scripts/generate.js
   // all "internal" components should be registered from their parent components
-  registerMgtAgendaComponent();
-  registerMgtContactComponent();
-  registerMgtFileComponent();
-  registerMgtFlyoutComponent();
-  registerMgtLoginComponent();
-  registerMgtPeopleComponent();
   registerMgtPersonComponent();
   registerMgtPersonCardComponent();
+  registerMgtAgendaComponent();
+  registerMgtGetComponent();
+  registerMgtLoginComponent();
+  registerMgtPeoplePickerComponent();
+  registerMgtPeopleComponent();
+  registerMgtTasksComponent();
+  registerMgtTeamsChannelPickerComponent();
+  registerMgtTodoComponent();
+  registerMgtFileComponent();
+  registerMgtFileListComponent();
+  registerMgtPickerComponent();
+  registerMgtTaxonomyPickerComponent();
+  registerMgtThemeToggleComponent();
+  registerMgtSearchBoxComponent();
+  registerMgtSearchResultsComponent();
   registerMgtSpinnerComponent();
 };
