112aaba9263791a49b21b8f85b5a4e955bfc5e86
Gavin Barron <gavinbarron@microsoft.com>
2023-10-24 04:25:40 -0700

    feat: add chat icons (#2789)
    adds icons for group chat and meeting chats
    fixes overall layout of chat header
    refactors ChatHeader in to smaller components
    adds a css property for aligning items in the mgt-person
    fixes padding around chat messages and sendbox
    update person style story with new css property
    set cursor to pointer for chat members list items
    fix z-index for chat name editing popover to ensure that dividers do not render above it
    change hard coded theme to web light
    fix missing edit icon for chat renames
    
    ---------
    
    Co-authored-by: Musale Martin <martinmusale@microsoft.com>

--- packages/mgt-chat/src/components/ChatHeader/ChatHeader.tsx
+++ packages/mgt-chat/src/components/ChatHeader/ChatHeader.tsx
@@ -1,152 +1,54 @@
-import React, { memo, useCallback, useState } from 'react';
-import { AadUserConversationMember, Chat } from '@microsoft/microsoft-graph-types';
-import { Edit20Filled, Edit20Regular, bundleIcon } from '@fluentui/react-icons';
-import { Person, PersonCardInteraction, ViewType } from '@microsoft/mgt-react';
-import {
-  Button,
-  Field,
-  Input,
-  InputOnChangeData,
-  OnOpenChangeData,
-  Popover,
-  PopoverSurface,
-  PopoverTrigger,
-  makeStyles,
-  tokens
-} from '@fluentui/react-components';
-import type { PopoverProps } from '@fluentui/react-components';
+import { Divider, makeStyles, mergeClasses } from '@fluentui/react-components';
+import React, { memo } from 'react';
+import { GraphChatClient } from '../../statefulClient/StatefulGraphChatClient';
+import ChatTitle from './ChatTitle';
+import { ManageChatMembers } from '../ManageChatMembers/ManageChatMembers';
+import { useCommonHeaderStyles } from './useCommonHeaderStyles';
 
-interface ChatHeaderProps {
-  chat?: Chat;
-  currentUserId?: string;
-  onRenameChat: (newName: string | null) => Promise<void>;
-}
-
-const EditIcon = bundleIcon(Edit20Filled, Edit20Regular);
-
-const useStyles = makeStyles({
+const useHeaderStyles = makeStyles({
   chatHeader: {
     display: 'flex',
-    alignItems: 'center',
-    columnGap: '4px',
-    lineHeight: '32px',
-    fontSize: '18px',
-    fontWeight: 700
-  },
-  container: {
-    display: 'flex',
     flexDirection: 'column',
-    gridRowGap: '16px',
-    minWidth: '300px',
-    backgroundColor: tokens.colorNeutralBackground1
+    rowGap: 0,
+    zIndex: 2
   },
-  formButtons: {
+  secondRow: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    gridColumnGap: '8px'
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    height: '30px'
+  },
+  noGrow: {
+    flexGrow: 0
   }
 });
-
-const reduceToFirstNamesList = (participants: AadUserConversationMember[] = [], userId = '') => {
-  return participants
-    .filter(p => p.userId !== userId)
-    .map(p => {
-      if (p.displayName?.includes(' ')) {
-        return p.displayName.split(' ')[0];
-      }
-      return p.displayName || p.email || p.id;
-    })
-    .join(', ');
-};
-
-const GroupChatHeader = ({ chat, currentUserId, onRenameChat }: ChatHeaderProps) => {
-  const styles = useStyles();
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  const handleOpenChange = useCallback((_, data: OnOpenChangeData) => setIsPopoverOpen(data.open || false), []);
-  const onCancelClicked = useCallback(() => setIsPopoverOpen(false), []);
-  const [chatName, setChatName] = useState(chat?.topic);
-  const onChatNameChanged = useCallback(
-    (_: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, data: InputOnChangeData) => {
-      setChatName(data.value);
-    },
-    []
-  );
-  const renameChat = useCallback(() => {
-    void onRenameChat(chatName || null);
-  }, [chatName, onRenameChat]);
-  const chatTitle = chat?.topic
-    ? chat.topic
-    : reduceToFirstNamesList(chat?.members as AadUserConversationMember[], currentUserId);
-  const trapFocus = true;
-  const popoverProps: Partial<PopoverProps> = {
-    trapFocus,
-    inertTrapFocus: trapFocus,
-    inline: true,
-    positioning: 'below-start',
-    open: isPopoverOpen,
-    onOpenChange: handleOpenChange
-  };
-  return (
-    <>
-      {chatTitle}
-      <Popover {...popoverProps}>
-        <PopoverTrigger>
-          <Button appearance="transparent" icon={<EditIcon />} aria-label="Name group chat"></Button>
-        </PopoverTrigger>
-        <PopoverSurface>
-          <div className={styles.container}>
-            <Field label="Group name">
-              <Input placeholder="Enter group name" onChange={onChatNameChanged} value={chatName || ''} />
-            </Field>
-            <div className={styles.formButtons}>
-              <Button appearance="secondary" onClick={onCancelClicked}>
-                Cancel
-              </Button>
-              <Button appearance="primary" disabled={chatName === chat?.topic} onClick={renameChat}>
-                Send
-              </Button>
-            </div>
-          </div>
-        </PopoverSurface>
-      </Popover>
-    </>
-  );
-};
-
-const OneToOneChatHeader = ({ chat, currentUserId }: ChatHeaderProps) => {
-  const id = getOtherParticipantUserId(chat, currentUserId);
-  return id ? (
-    <Person
-      userId={id}
-      view={ViewType.oneline}
-      avatarSize="small"
-      personCardInteraction={PersonCardInteraction.hover}
-      showPresence={true}
-    />
-  ) : null;
-};
-
-const getOtherParticipantUserId = (chat?: Chat, currentUserId = '') =>
-  (chat?.members as AadUserConversationMember[])?.find(m => m.userId !== currentUserId)?.userId;
-
-/**
- * Shows the "name" of the chat.
- * For group chats where the topic is set it will show the topic.
- * For group chats with no topic it will show the first names of other participants comma separated.
- * For 1:1 chats it will show the first name of the other participant.
- */
-const ChatHeader = ({ chat, currentUserId, onRenameChat }: ChatHeaderProps) => {
-  const styles = useStyles();
+interface ChatHeaderProps {
+  chatState: GraphChatClient;
+}
+const ChatHeader = ({ chatState }: ChatHeaderProps) => {
+  const styles = useHeaderStyles();
+  const commonStyles = useCommonHeaderStyles();
   return (
     <div className={styles.chatHeader}>
-      {chat?.chatType === 'group' ? (
-        <GroupChatHeader chat={chat} currentUserId={currentUserId} onRenameChat={onRenameChat} />
-      ) : (
-        <OneToOneChatHeader chat={chat} currentUserId={currentUserId} onRenameChat={onRenameChat} />
-      )}
+      <ChatTitle chat={chatState.chat} currentUserId={chatState.userId} onRenameChat={chatState.onRenameChat} />
+      <Divider appearance="subtle" />
+      <div className={mergeClasses(styles.secondRow, commonStyles.row)}>
+        {chatState.participants?.length > 0 && chatState.chat?.chatType === 'group' && (
+          <>
+            <ManageChatMembers
+              members={chatState.participants}
+              removeChatMember={chatState.onRemoveChatMember}
+              currentUserId={chatState.userId}
+              addChatMembers={chatState.onAddChatMembers}
+            />
+            <Divider vertical appearance="subtle" inset className={styles.noGrow} />
+          </>
+        )}
+      </div>
+      <Divider appearance="subtle" />
     </div>
   );
 };
 
-export default memo(ChatHeader);
+export { ChatHeader };
