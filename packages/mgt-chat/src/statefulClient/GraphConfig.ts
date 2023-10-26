042dcb1fae4ead9b30dee062b4665201f1ed65e7
Gavin Barron <gavinbarron@microsoft.com>
2023-10-26 21:27:55 +0000

    feat: add config switch to use cananry graph

--- packages/mgt-chat/src/statefulClient/GraphConfig.ts
+++ packages/mgt-chat/src/statefulClient/GraphConfig.ts
@@ -5,24 +5,27 @@ export class GraphConfig {
 
   public static version = 'v1.0';
 
-  // public static subscriptionConnectionVersion = 'testprodv1.0e2ewebsockets';
+  public static canarySubscriptionVersion = 'testprodv1.0e2ewebsockets';
+
+  public static webSocketsPrefix = 'websockets:';
 
   public static get graphEndpoint(): GraphEndpoint {
-    // return GraphConfig.useCanary ? 'https://canary.graph.microsoft.com' :
-    return 'https://graph.microsoft.com';
+    return GraphConfig.useCanary ? 'https://canary.graph.microsoft.com' : 'https://graph.microsoft.com';
+  }
+
+  public static get baseCanaryUrl(): string {
+    return `${GraphConfig.graphEndpoint}/${this.canarySubscriptionVersion}`;
   }
 
   public static get subscriptionEndpoint(): string {
-    return '/subscriptions';
+    return GraphConfig.useCanary ? `${GraphConfig.baseCanaryUrl}/subscriptions` : '/subscriptions';
   }
 
-  // public static adjustNotificationUrl(url: string): string {
-  //   let temp = url;
-  //   if (GraphConfig.useCanary && url) {
-  //     temp = url.replace('https://graph.microsoft.com/1.0', GraphConfig.baseCanaryUrl);
-  //     temp = temp.replace('https://graph.microsoft.com/beta', GraphConfig.baseCanaryUrl);
-  //   }
-  //   temp = temp.replace(GraphConfig.version, GraphConfig.subscriptionConnectionVersion);
-  //   return temp.replace('wss:', '');
-  // }
+  public static adjustNotificationUrl(url: string): string {
+    if (GraphConfig.useCanary && url) {
+      url = url.replace('https://graph.microsoft.com/1.0', GraphConfig.baseCanaryUrl);
+      url = url.replace('https://graph.microsoft.com/beta', GraphConfig.baseCanaryUrl);
+    }
+    return url.replace(GraphConfig.webSocketsPrefix, '');
+  }
 }
