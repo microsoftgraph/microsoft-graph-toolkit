582d5eca684d49c333a5d47bfa36eebc6d4604be
Gavin Barron <gavinbarron@microsoft.com>
2023-11-10 16:34:54 +0000

    feat!: split static config and permission methods out of mgt-personon-card
    splits out the logic for cacluating the scopes required for the mgt-person-card into a separate function and file
    splits the static config for MgtPersonCard into a separate class
    changes the schema of the config for MgtPersonCard
    fixes an issue where setting isSendMessageVisible to false did not stop the quick message ui from rendering
    adds tests to validate the permission sets
    
    BREAKING CHANGE: MgtPersonCard no longer has a static config property.This config has been moved to the MgtPersonCardConfig class to allow developers to import the config and associated getMgtPersonCardScopes function at the top level of their applicaiton without automatically adding the weight of the full mgt-person-card component and dependencies to the entry file for their applications.
    BREAKING CHANGE: The organization section for configuring MgtPersonCard may no longer be set to false, use undefined instead.

---
+++ packages/mgt-components/src/components/mgt-person-card/MgtPersonCardConfig.ts
@@ -0,0 +1,48 @@
+import { IMgtPersonCardConfig } from './mgt-person-card.types';
+
+interface SectionsConfig {
+  /**
+   * Gets or sets whether the organization section is shown
+   *
+   */
+  organization?: {
+    /**
+     * Gets or sets whether the "Works with" section is shown
+     *
+     * @type {boolean}
+     */
+    showWorksWith: boolean;
+  };
+
+  /**
+   * Gets or sets whether the messages section is shown
+   *
+   * @type {boolean}
+   */
+  mailMessages: boolean;
+
+  /**
+   * Gets or sets whether the files section is shown
+   *
+   * @type {boolean}
+   */
+  files: boolean;
+
+  /**
+   * Gets or sets whether the profile section is shown
+   *
+   * @type {boolean}
+   */
+  profile: boolean;
+}
+
+export class MgtPersonCardConfig {
+  public static sections = {
+    files: true,
+    mailMessages: true,
+    organization: { showWorksWith: true },
+    profile: true
+  };
+  public static useContactApis = true;
+  public static isSendMessageVisible = true;
+}
