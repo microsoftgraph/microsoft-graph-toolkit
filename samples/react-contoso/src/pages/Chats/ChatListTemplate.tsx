3c71e9187df3006be70b9de612bc02c9ac7b4acb
Sébastien Levert <sebastienlevert@users.noreply.github.com>
2023-11-13 19:46:05 -0500

    chore: updating the chat list component for performance and design (#2838)
    * Update chat list behavior and look and feel
    
    * Review comments
    
    * Updating deps.
    
    * refactor of resourceUrl calculation to common function
    
    * refactor to ensure a proper name for the memoized component
    
    ---------
    
    Co-authored-by: Gavin Barron <gavinbarron@microsoft.com>

--- samples/react-contoso/src/pages/Chats/ChatListTemplate.tsx
+++ samples/react-contoso/src/pages/Chats/ChatListTemplate.tsx
@@ -1,26 +1,68 @@
 import React, { useState } from 'react';
-import { MgtTemplateProps } from '@microsoft/mgt-react';
+import { MgtTemplateProps, Providers } from '@microsoft/mgt-react';
 import { Chat } from '@microsoft/microsoft-graph-types';
-import ChatItem, { ChatInteractionProps } from './ChatItem';
+import { ChatItem } from './ChatItem';
 import { Chat as GraphChat } from '@microsoft/microsoft-graph-types';
+import { List, ListItem } from '@fluentui/react-northstar';
+import { makeStyles, shorthands } from '@fluentui/react-components';
+import { iconFilledClassName, iconRegularClassName } from '@fluentui/react-icons';
+
+const useStyles = makeStyles({
+  listItem: {
+    listStyleType: 'none',
+    width: '100%',
+    ':focus-visible': {
+      [`& .${iconFilledClassName}`]: {
+        display: 'inline',
+        color: 'var(--colorNeutralForeground2BrandHover)'
+      },
+      [`& .${iconRegularClassName}`]: {
+        display: 'none'
+      }
+    }
+  },
+  selected: {
+    backgroundColor: 'var(--colorNeutralBackground1Selected)'
+  },
+  list: {
+    fontWeight: 800,
+    gridGap: '8px',
+    ...shorthands.marginBlock('0'),
+    ...shorthands.padding('0')
+  }
+});
+
+export interface ChatInteractionProps {
+  onChatSelected: (selected: Chat) => void;
+}
 
 const ChatListTemplate = (props: MgtTemplateProps & ChatInteractionProps) => {
+  const styles = useStyles();
   const { value } = props.dataContext;
-  const chats: Chat[] = value;
-  const [selectedChat, setSelectedChat] = useState<GraphChat>(props.selectedChat || chats[0]);
+  const [chats] = useState<Chat[]>((value as Chat[]).filter(c => c.members?.length! > 1));
+  const [userId, setUserId] = useState<string>();
+  const [selectedChat, setSelectedChat] = useState<GraphChat | undefined>(chats.length > 0 ? chats[0] : undefined);
 
-  const onChatSelected = React.useCallback(
-    (e: GraphChat) => {
-      setSelectedChat(e);
-      props.onSelected(selectedChat);
-    },
-    [setSelectedChat, selectedChat, props]
-  );
+  const onChatSelected = (e: GraphChat) => {
+    setSelectedChat(e);
+  }
 
   // Set the selected chat to the first chat in the list
   // Fires only the first time the component is rendered
   React.useEffect(() => {
-    onChatSelected(selectedChat);
+    if (selectedChat) {
+      props.onChatSelected(selectedChat);
+    }
+  }, [props, selectedChat]);
+
+  React.useEffect(() => {
+    const getMyId = async () => {
+      const me = await Providers.me();
+      setUserId(me.id);
+    };
+    if (!userId) {
+      void getMyId();
+    }
   });
 
   const isChatActive = (chat: Chat) => {
@@ -31,18 +73,26 @@ const ChatListTemplate = (props: MgtTemplateProps & ChatInteractionProps) => {
     return false;
   };
 
-  console.log('chats', chats);
   return (
-    <div>
+    <List navigable className={styles.list}>
       {chats.map((c, index) => (
-        <ChatItem
+        <ListItem
           key={c.id}
-          chat={c}
-          isSelected={(!selectedChat && index === 0) || isChatActive(c)}
-          onSelected={onChatSelected}
-        />
+          index={index}
+          className={styles.listItem}
+          content={
+            <div className={(!selectedChat && index === 0) || isChatActive(c) ? styles.selected : ''}>
+              <ChatItem
+                key={c.id}
+                chat={c}
+                userId={userId}
+              />
+            </div>
+          }
+          onClick={() => onChatSelected(c)}>
+        </ListItem>
       ))}
-    </div>
+    </List>
   );
 };
 
