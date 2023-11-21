d14b593ca85f2cfcf0242244f3477a60c1292d45
Martin Musale <martinmusale@microsoft.com>
2023-11-21 16:49:29 +0300

    Change emoji translation to standard HTML
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- packages/mgt-chat/src/statefulClient/StatefulGraphChatClient.ts
+++ packages/mgt-chat/src/statefulClient/StatefulGraphChatClient.ts
@@ -60,7 +60,7 @@ import {
 } from './graph.chat';
 import { updateMessageContentWithImage } from '../utils/updateMessageContentWithImage';
 import { isChatMessage } from '../utils/types';
-import { rewriteEmojiContent } from '../utils/rewriteEmojiContent';
+import { rewriteEmojiContentToHTML } from '../utils/rewriteEmojiContent';
 
 // 1x1 grey pixel
 const placeholderImageContent =
@@ -1016,8 +1016,7 @@ detail: ${JSON.stringify(eventDetail)}`);
     }
     let content = graphMessage.body?.content ?? 'undefined';
     let result: MessageConversion = {};
-    // do simple emoji replacement first
-    content = rewriteEmojiContent(content);
+    content = rewriteEmojiContentToHTML(content);
     // Handle any mentions in the content
     content = this.updateMentionsContent(content);
 
--- packages/mgt-chat/src/utils/rewriteEmojiContent.ts
+++ packages/mgt-chat/src/utils/rewriteEmojiContent.ts
@@ -13,11 +13,11 @@ const emojiMatch = (messageContent: string): RegExpMatchArray | null => {
 };
 // iterative repave the emoji custom element with the content of the alt attribute
 // on the emoji element
-const processEmojiContent = (messageContent: string): string => {
+const processEmojiContent = (messageContent: string, replacer = '$2'): string => {
   let result = messageContent;
   let match = emojiMatch(result);
   while (match) {
-    result = result.replace(emojiRegex, '$2');
+    result = result.replace(emojiRegex, replacer);
     match = emojiMatch(result);
   }
   return result;
@@ -30,3 +30,34 @@ const processEmojiContent = (messageContent: string): string => {
  */
 export const rewriteEmojiContent = (content: string): string =>
   emojiMatch(content) ? processEmojiContent(content) : content;
+
+const emojiHtmlString = (emoji: string, title: string, hasOtherContent?: boolean): string => {
+  const strippedTitle = title.replace(/\s+/g, '').toLowerCase();
+  const size = hasOtherContent ? '20' : '50';
+  return `
+    <span contenteditable="false" title="${title}" type="(${strippedTitle})" class="animated-emoticon-${size}-${strippedTitle}">
+      <img
+        itemscope=""
+        itemtype="http://schema.skype.com/Emoji"
+        itemid="${strippedTitle}"
+        src="https://statics.teams.cdn.office.net/evergreen-assets/personal-expressions/v2/assets/emoticons/${strippedTitle}/default/${size}_f.png"
+        title="${title}"
+        alt="${emoji}"
+        style="width:${size}px;height:${size}px;" />
+    </span>`;
+};
+const emojiRegexForHtml = /(<emoji[^>]+)alt=["'](\w*[^"']*)["']\s+title=["'](\w*[^"']*)["']><\/emoji>/g;
+
+export const rewriteEmojiContentToHTML = (content: string): string => {
+  const matches = content.matchAll(emojiRegexForHtml);
+  let finalContent = '';
+  for (const match of matches) {
+    const emoji = match[2];
+    const title = match[3];
+    const htmlString = emojiHtmlString(emoji, title, false);
+    finalContent = processEmojiContent(content, htmlString);
+    console.log({ emoji, title, htmlString });
+  }
+  if (finalContent) return finalContent;
+  return content;
+};
