11b821b5d67aa92312005e9efe6ccdfd5a4eb38d
Martin Musale <martinmusale@microsoft.com>
2023-11-28 12:10:41 +0300

    Refactor the callback messages with minimal types
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- packages/mgt-chat/src/components/NewChat/NewChat.tsx
+++ packages/mgt-chat/src/components/NewChat/NewChat.tsx
@@ -66,20 +66,15 @@ const NewChat: FC<NewChatProps> = ({ mode = 'auto', onChatCreated, onCancelClick
   );
   // chat name data control
   const [chatName, setChatName] = useState<string>('');
-  const onChatNameChanged = useCallback(
-    (_: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, data: InputOnChangeData) => {
-      setChatName(data.value);
-    },
-    []
-  );
+  const onChatNameChanged = useCallback((_: React.FormEvent<HTMLInputElement>, data: InputOnChangeData) => {
+    setChatName(data.value);
+  }, []);
+
   // initial message data control
   const [initialMessage, setInitialMessage] = useState<string>('');
-  const onInitialMessageChange = useCallback(
-    (event: React.FormEvent<HTMLTextAreaElement>, data: TextareaOnChangeData) => {
-      setInitialMessage(data.value);
-    },
-    []
-  );
+  const onInitialMessageChange = useCallback((_: React.FormEvent<HTMLTextAreaElement>, data: TextareaOnChangeData) => {
+    setInitialMessage(data.value);
+  }, []);
   const createChat = useCallback(() => {
     const graphClient: IGraph = graph('mgt-new-chat');
     setState('creating chat');
