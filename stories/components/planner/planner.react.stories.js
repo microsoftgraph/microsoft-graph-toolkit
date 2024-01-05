842cc90a14cfb2b6c8919cc06ab49c8c7344ca84
Sébastien Levert <slevert@microsoft.com>
2024-01-05 16:42:31 +0000

    Adding planner

--- stories/components/planner/planner.react.stories.js
+++ stories/components/planner/planner.react.stories.js
@@ -9,25 +9,18 @@ import { html } from 'lit';
 import { withCodeEditor } from '../../../.storybook/addons/codeEditorAddon/codeAddon';
 
 export default {
-  title: 'Components / mgt-planner',
+  title: 'Components / mgt-planner / React',
   component: 'planner',
-  decorators: [withCodeEditor],
-  tags: ['autodocs'],
-  parameters: {
-    docs: {
-      source: { code: '<mgt-planner></mgt-planner>' }
-    }
-  }
+  decorators: [withCodeEditor]
 };
 
-export const tasks = () => html`
+export const planner = () => html`
   <mgt-planner></mgt-planner>
-`;
+<react>
+import { Planner } from '@microsoft/mgt-react';
 
-export const tasksWithGroupId = () => html`
-  <mgt-planner group-id="45327068-6785-4073-8553-a750d6c16a45"></mgt-planner>
-  <!--
-    NOTE: the default sandbox tenant doesn't have the required Tasks.ReadWrite and
-    Group.ReadWrite.All permissions. Test this component in your tenant.
-  -->
+export default () => (
+  <Planner></Planner>
+);
+</react>
 `;
