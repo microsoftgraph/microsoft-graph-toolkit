458da6597d8da2f143cbf804c45cbe84a6bef69d
ENG KEONG DAVID LEE <36802330+seekdavidlee@users.noreply.github.com>
2024-01-08 09:50:58 -0600

    layout component - Stub out all visual components. (#2)

--- packages/mgt-chat/src/components/ChatList/ChatList.tsx
+++ packages/mgt-chat/src/components/ChatList/ChatList.tsx
@@ -1,17 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import { ChatListItem, IChatListItemInteractionProps } from '../ChatListItem/ChatListItem';
-import { SampleChats } from '../ChatListItem/sampleData';
 import { MgtTemplateProps } from '@microsoft/mgt-react';
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
+import { makeStyles, Link, FluentProvider, shorthands, webLightTheme } from '@fluentui/react-components';
 import { FluentThemeProvider } from '@azure/communication-react';
 import { FluentTheme } from '@fluentui/react';
 import { Chat as GraphChat } from '@microsoft/microsoft-graph-types';
 import { StatefulGraphChatClient } from '../../statefulClient/StatefulGraphChatClient';
 import { useGraphChatClient } from '../../statefulClient/useGraphChatClient';
-import { makeStyles, shorthands } from '@fluentui/react-components';
+import { ChatListHeader } from '../ChatListHeader/ChatListHeader';
+import { IChatListMenuItemsProps } from '../ChatListHeader/EllipsisMenu';
+import { ChatListButtonItem } from '../ChatListHeader/ChatListButtonItem';
+
+const useStyles = makeStyles({
+  headerContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    ...shorthands.padding('10px')
+  },
+  linkContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    ...shorthands.padding('10px')
+  },
+  loadMore: {
+    textDecorationLine: 'none',
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+    '&:hover': {
+      textDecorationLine: 'none' // This removes the underline when hovering
+    }
+  }
+});
 
 // this is a stub to move the logic here that should end up here.
-export const ChatList = (props: MgtTemplateProps & IChatListItemInteractionProps) => {
+export const ChatList = (
+  props: MgtTemplateProps &
+    IChatListItemInteractionProps &
+    IChatListMenuItemsProps & {
+      buttonItems?: ChatListButtonItem[];
+    }
+) => {
+  const styles = useStyles();
   // TODO: change this to use StatefulGraphChatListClient
   const chatClient: StatefulGraphChatClient = useGraphChatClient('');
   const [chatState, setChatState] = useState(chatClient.getState());
@@ -25,13 +56,30 @@ export const ChatList = (props: MgtTemplateProps & IChatListItemInteractionProps
   const { value } = props.dataContext as { value: GraphChat[] };
   const chats: GraphChat[] = value;
 
+  const chatListButtonItems = props.buttonItems === undefined ? [] : props.buttonItems;
+  const chatListMenuItems = props.menuItems === undefined ? [] : props.menuItems;
+  chatListMenuItems.unshift({
+    displayText: 'Mark all as read',
+    onClick: () => {}
+  });
+
   return (
     // This is a temporary approach to render the chatlist items. This should be replaced.
     <FluentThemeProvider fluentTheme={FluentTheme}>
       <FluentProvider theme={webLightTheme}>
-        {chats.map(c => (
-          <ChatListItem key={c.id} chat={c} myId={chatState.userId} onSelected={props.onSelected} />
-        ))}
+        <div>
+          <div className={styles.headerContainer}>
+            <ChatListHeader buttonItems={chatListButtonItems} menuItems={chatListMenuItems} />
+          </div>
+          {chats.map(c => (
+            <ChatListItem key={c.id} chat={c} myId={chatState.userId} onSelected={props.onSelected} />
+          ))}
+          <div className={styles.linkContainer}>
+            <Link href="#" className={styles.loadMore}>
+              load more
+            </Link>
+          </div>
+        </div>
       </FluentProvider>
     </FluentThemeProvider>
   );
