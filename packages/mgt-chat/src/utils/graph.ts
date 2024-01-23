403f5ab12c0f1c131e58c17d015057d4c6e784d9
Martin Musale <martinmusale@microsoft.com>
2024-01-23 11:55:03 +0300

    Remove comment as it has a fix to delay setting the graph
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- packages/mgt-chat/src/utils/graph.ts
+++ packages/mgt-chat/src/utils/graph.ts
@@ -9,7 +9,5 @@ import { IGraph, Providers } from '@microsoft/mgt-element';
 
 export const graph = (component: string, version?: string): IGraph => {
   if (!component) throw new Error('Component name is required');
-  // NOTE: for some unknown reason, the global provider is not available on first
-  // load. A delay is required to ensure that it is instatiated here.
   return Providers.globalProvider?.graph.forComponent(component, version);
 };
