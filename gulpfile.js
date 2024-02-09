b1717077757e545bd51860c87b491276ee87fa22
Gavin Barron <gavinbarron@microsoft.com>
2024-02-09 00:15:34 +0000

    chore: remove gulp as dev dep

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const gap = require('gulp-append-prepend');
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
 const license = require('gulp-header-license');
-const replace = require('gulp-replace-task');
 
 scssFileHeader = `
 // THIS FILE IS AUTO GENERATED
@@ -36,30 +31,6 @@ versionFile = `${licenseStr}
 export const PACKAGE_VERSION = '[VERSION]';
 `;
 
-function runSass() {
-  return (
-    gulp
-      .src('src/**/!(shared)*.scss')
-      .pipe(sass())
-      .pipe(cleanCSS())
-      // replacement to make office-ui-fabric-core icons work with lit-element
-      .pipe(
-        replace({
-          patterns: [
-            {
-              match: /:"\\([0-9a-f])/g,
-              replacement: ':"\\u$1'
-            }
-          ]
-        })
-      )
-      .pipe(gap.prependText(scssFileHeader))
-      .pipe(gap.appendText(scssFileFooter))
-      .pipe(rename({ extname: '-css.ts' }))
-      .pipe(gulp.dest('src/'))
-  );
-}
-
 function setLicense() {
   return gulp
     .src(['packages/**/src/**/*.{ts,js,scss}', '!packages/**/generated/**/*'], { base: './' })
@@ -73,11 +44,5 @@ function setVersion() {
   fs.writeFileSync('./src/utils/version.ts', versionFile.replace('[VERSION]', pkg.version));
 }
 
-gulp.task('sass', runSass);
 gulp.task('setLicense', setLicense);
 gulp.task('setVersion', async () => setVersion());
-
-gulp.task('watchSass', () => {
-  runSass();
-  return gulp.watch('src/**/*.scss', gulp.series('sass'));
-});
