382f15b6bae4f71f9401dfecbd7219c764623479
Martin Musale <martinmusale@microsoft.com>
2024-02-28 13:54:42 +0300

    Update the tests

--- packages/mgt-element/src/utils/signedInState.tests.ts
+++ packages/mgt-element/src/utils/signedInState.tests.ts
@@ -11,22 +11,22 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import { expect } from '@open-wc/testing';
+import { assert, expect } from '@open-wc/testing';
 import { signedInState } from './signedInState';
 import { MockProvider } from '../mock/MockProvider';
 import { Providers } from '../providers/Providers';
 import { ProviderState } from '../providers/IProvider';
 
 describe('signedInState', () => {
-  it('should be false', (): void => {
+  beforeEach(() => {
     Providers.globalProvider = new MockProvider(true);
-    expect(signedInState()).to.be.false;
   });
 
+  it('should be false', () => expect(signedInState()).to.be.false);
+
   it('should be true', () => {
-    Providers.globalProvider = new MockProvider(true);
     const provider = Providers.globalProvider;
     provider.setState(ProviderState.SignedIn);
-    expect(signedInState()).to.be.true;
+    assert(signedInState());
   });
 });
