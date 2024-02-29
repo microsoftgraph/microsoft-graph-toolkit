e539418aee83cebf46c303b767a9f5739be7a6c1
Musale Martin <martinmusale@microsoft.com>
2024-02-29 13:46:37 +0300

    Update packages/mgt-element/src/utils/signedInState.tests.ts
    Co-authored-by: Gavin Barron <gavinbarron@microsoft.com>

--- packages/mgt-element/src/utils/signedInState.tests.ts
+++ packages/mgt-element/src/utils/signedInState.tests.ts
@@ -4,12 +4,6 @@
  * See License in the project root for license information.
  * -------------------------------------------------------------------------------------------
  */
-/**
- * -------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.
- * See License in the project root for license information.
- * -------------------------------------------------------------------------------------------
- */
 
 import { assert, expect } from '@open-wc/testing';
 import { signedInState } from './signedInState';
