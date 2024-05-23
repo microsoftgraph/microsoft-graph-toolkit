dae3f232ee4ed3dbf86fc6dc2eff2100b2096239
Mnickii <miaronkirote@gmail.com>
2024-05-23 15:55:52 +0300

    add aria-controlls to input field

--- packages/mgt-components/src/components/mgt-people-picker/mgt-people-picker.ts
+++ packages/mgt-components/src/components/mgt-people-picker/mgt-people-picker.ts
@@ -692,7 +692,10 @@ export class MgtPeoplePicker extends MgtTemplatedTaskComponent {
         appearance="outline"
         slot="anchor"
         id="people-picker-input"
+        aria-controls="suggestions-list"
         role="combobox"
+        aria-live="assertive"
+        aria-atomic="true"
         placeholder=${this.hasMaxSelections ? this.strings.maxSelectionsPlaceHolder : placeholder}
         aria-label=${this.ariaLabel || maxSelectionsAriaLabel || placeholder || this.strings.selectContact}
         aria-expanded=${this.flyout?.isOpen ?? false}
@@ -857,6 +860,7 @@ export class MgtPeoplePicker extends MgtTemplatedTaskComponent {
         class="searched-people-list"
         role="listbox"
         aria-live="polite"
+        aria-label="${this.strings.suggestionsTitle}"
         title=${this.strings.suggestionsTitle}
       >
         ${repeat(
