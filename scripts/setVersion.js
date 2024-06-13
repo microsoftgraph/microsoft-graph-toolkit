9fa2fbbf6413e876b3225a1946c8d8cdb8690b08
Mnickii <miaronkirote@gmail.com>
2024-06-13 12:15:18 +0300

    Revert "bump versions"
    This reverts commit 2c88449761e1a31fa6ee5c80c246df78509a3fbc.

--- scripts/setVersion.js
+++ scripts/setVersion.js
@@ -28,28 +28,19 @@ const getFiles = (filter, startPath = 'packages') => {
 };
 
 const updateMgtDependencyVersion = (packages, version) => {
-  for (let item of packages) {
-    console.log(`updating package ${item} with version ${version}`);
-    const data = fs.readFileSync(item, 'utf8');
+  for (let package of packages) {
+    console.log(`updating package ${package} with version ${version}`);
+    const data = fs.readFileSync(package, 'utf8');
 
-    let result = data.replace(/"(@microsoft\/mgt.*)": "(\*)"/g, `"$1": "${version}"`);
-    result = result.replace(/"version": "(.*)"/g, `"version": "${version}"`);
+    let result;
+    if (package === 'sonar-project.properties') {
+      result = data.replace(/sonar.projectVersion=(.*)/g, `sonar.projectVersion=${version}`);
+    } else {
+      result = data.replace(/"(@microsoft\/mgt.*)": "(\*)"/g, `"$1": "${version}"`);
+      result = result.replace(/"version": "(.*)"/g, `"version": "${version}"`);
+    }
 
-    fs.writeFileSync(item, result, 'utf8');
-  }
-};
-
-const updateSonarProjectVersion = () => {
-  const rootDir = path.resolve(__dirname, '..');
-  const sonarFilePath = path.join(rootDir, 'sonar-project.properties');
-
-  if (fs.existsSync(sonarFilePath)) {
-    const data = fs.readFileSync(sonarFilePath, 'utf8');
-    const result = data.replace(/sonar.projectVersion=(.*)/g, `sonar.projectVersion=${version}`);
-    fs.writeFileSync(sonarFilePath, result, 'utf8');
-  } else {
-    console.log('sonar-project.properties file not found in root directory');
-    return null;
+    fs.writeFileSync(package, result, 'utf8');
   }
 };
 
@@ -91,4 +82,5 @@ const packages = getFiles('package.json', '.');
 updateMgtDependencyVersion(packages, version);
 
 // include update to sonar-project.properties
-updateSonarProjectVersion();
+const sonarProperties = getFiles('sonar-project.properties', '.');
+updateMgtDependencyVersion(sonarProperties, version);
