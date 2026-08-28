97da910e4cf35add77384f8a3102cbb84f4d6cd3
Gavin Barron <gavinbarron@microsoft.com>
2026-08-28 10:46:16 -0700

    Revert "Deploying to gh-pages from @ microsoftgraph/microsoft-graph-toolkit@d069f98aa21281abc0cb6eb324a94d7fe3dc9144 🚀"
    This reverts commit e8526a5a66e306c2f3fb530158a4b11173fbf114.

--- next/stackblitz/html/app.js
+++
@@ -1,4 +0,0 @@
-import { registerMgtComponents, Providers, MockProvider } from './node_modules/@microsoft/mgt/dist/es6/index.js';
-
-Providers.globalProvider = new MockProvider(true);
-registerMgtComponents();
